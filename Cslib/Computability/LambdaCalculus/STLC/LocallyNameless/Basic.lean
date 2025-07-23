194e11d401dd71397b75db2c0a8c4defc4548c81
Chris Henson <chrishenson.net@gmail.com>
2025-07-23 17:26:24 -0400

    restructure for clarity

--- Cslib/Computability/LambdaCalculus/STLC/LocallyNameless/Basic.lean
+++ Cslib/Computability/LambdaCalculus/STLC/LocallyNameless/Basic.lean
@@ -5,7 +5,7 @@ Authors: Chris Henson
 -/
 
 import Cslib.Computability.LambdaCalculus.STLC.LocallyNameless.Context
-import Cslib.Computability.LambdaCalculus.Untyped.LocallyNameless.Basic
+import Cslib.Computability.LambdaCalculus.Untyped.LocallyNameless.Properties
 
 /-! # λ-calculus
 
@@ -48,3 +48,105 @@ inductive Typing : Ctx Var Ty → Term Var → Ty → Prop
 | app : Typing Γ t (σ ⤳ τ) → Typing Γ t' σ → Typing Γ (app t t') τ
 
 scoped notation:50 Γ " ⊢ " t " ∶" τ:arg => Typing Γ t τ
+
+namespace Typing
+
+variable {Γ Δ Φ : Ctx Var Ty}
+
+/-- Typing is preserved on permuting a context. -/
+theorem perm : Γ.Perm Δ → Γ ⊢ t ∶ τ → Δ ⊢ t ∶ τ := by 
+  intros _ der
+  revert Δ
+  induction der <;> intros Δ p
+  case app => aesop
+  case var => 
+    have := @p.mem_iff
+    aesop
+  case abs ih => 
+    constructor
+    intros x mem
+    exact ih x mem (by aesop)
+
+/-- Weakening of a typing derivation with an appended context. -/
+lemma weakening_strengthened : 
+    Γ ++ Δ ⊢ t ∶ τ → (Γ ++ Φ ++ Δ).Ok → (Γ ++ Φ ++ Δ) ⊢ t ∶ τ := by
+  generalize eq : Γ ++ Δ = Γ_Δ
+  intros h
+  revert Γ Δ Φ
+  induction h <;> intros Γ Δ Φ eq ok_Γ_Φ_Δ
+  case var => aesop
+  case app => aesop
+  case abs σ Γ' τ t xs ext ih =>
+    apply Typing.abs (xs ∪ (Γ ++ Φ ++ Δ).dom)
+    intros x _
+    have h : (x, σ) :: Γ ++ Δ = (x, σ) :: Γ' := by aesop
+    refine @ih x (by aesop) _ _ Φ h ?_
+    constructor <;> aesop
+
+/-- Weakening of a typing derivation by an additional context. -/
+lemma weakening : Γ ⊢ t ∶ τ → (Γ ++ Δ).Ok → Γ ++ Δ ⊢ t ∶ τ := by
+  intros der ok
+  rw [←List.append_nil (Γ ++ Δ)] at *
+  exact weakening_strengthened (by simp_all) ok
+
+/-- Typing derivations exist only for locally closed terms. -/
+lemma lc : Γ ⊢ t ∶ τ → t.LC := by
+  intros h
+  induction h <;> constructor
+  case abs ih => exact ih
+  all_goals aesop
+
+variable [HasFresh Var]
+
+open Term
+
+/-- Substitution for a context weakened by a single type between appended contexts. -/
+lemma subst_strengthened :
+    (Δ ++ (x, σ) :: Γ) ⊢ t ∶ τ →
+    Γ ⊢ s ∶ σ → 
+    (Δ ++ Γ) ⊢ (t [x := s]) ∶ τ := by
+  generalize  eq : Δ ++ (x, σ) :: Γ = Φ
+  intros h
+  revert Γ Δ
+  induction h <;> intros Γ Δ eq der
+  case app => aesop
+  case var x' τ ok mem => 
+    simp only [subst_fvar]
+    rw [←eq] at mem
+    rw [←eq] at ok
+    cases (Ctx.perm (by aesop) ok : @Ctx.Ok Var Ty _ ((x, σ) :: Δ ++ Γ))
+    case cons ok_weak _ =>
+    observe perm : (Γ ++ Δ).Perm (Δ ++ Γ)
+    by_cases h : x = x' <;> simp only [h]
+    · rw [show τ = σ by aesop]
+      refine Typing.perm perm (weakening der ?_)
+      exact Ctx.perm (id (List.Perm.symm perm)) ok_weak
+    · aesop
+  case abs σ Γ' t T2 xs ih' ih =>
+    apply Typing.abs (xs ∪ {x} ∪ (Δ ++ Γ).dom)
+    intros x _
+    rw [
+      subst_def, 
+      subst_open_var _ _ _ _ _ der.lc,
+      show (x, σ) :: (Δ ++ Γ) = ((x, σ) :: Δ) ++ Γ by aesop
+      ]
+    apply ih 
+    all_goals aesop  
+
+/-- Substitution for a context weakened by a single type. -/
+lemma typing_subst : 
+    (x, σ) :: Γ ⊢ t ∶ τ  → Γ ⊢ s ∶ σ → Γ ⊢ (t [x := s]) ∶ τ := by
+  intros weak der
+  rw [←List.nil_append Γ]
+  exact subst_strengthened weak der
+
+/-- Typing preservation for opening. -/
+@[aesop safe forward (rule_sets := [LambdaCalculus.LocallyNameless.ruleSet])]
+theorem preservation_opening {xs : Finset Var} :
+  (∀ x ∉ xs, (x, σ) :: Γ ⊢ m ^ fvar x ∶ τ) → 
+  Γ ⊢ n ∶ σ → Γ ⊢ m ^ n ∶ τ
+  := by
+  intros mem der
+  have ⟨fresh, free⟩ := fresh_exists (xs ∪ m.fv)
+  rw [subst_intro fresh n m (by aesop) der.lc]
+  refine typing_subst (mem fresh (by aesop)) der
--- Cslib/Computability/LambdaCalculus/STLC/LocallyNameless/Safety.lean
+++ Cslib/Computability/LambdaCalculus/STLC/LocallyNameless/Safety.lean
@@ -11,7 +11,8 @@ import Cslib.Computability.LambdaCalculus.Untyped.LocallyNameless.FullBetaConflu
 
 /-! # λ-calculus
 
-Typing safety of the simply typed λ-calculus, with a locally nameless representation of syntax.
+Type safety of the simply typed λ-calculus, with a locally nameless representation of syntax.
+Theorems in this file are namespaced by their respective reductions.
 
 ## References
 
@@ -23,117 +24,33 @@ Typing safety of the simply typed λ-calculus, with a locally nameless represent
 
 universe u
 
-variable {Var : Type u} [DecidableEq Var]
-
-namespace LambdaCalculus.LocallyNameless.STLC.Typing
-
-variable {Γ Δ Φ : Ctx Var Ty}
-
-/-- Typing is preserved on permuting a context. -/
-theorem perm : Γ.Perm Δ → Γ ⊢ t ∶ τ → Δ ⊢ t ∶ τ := by 
-  intros _ der
-  revert Δ
-  induction der <;> intros Δ p
-  case app => aesop
-  case var => 
-    have := @p.mem_iff
-    aesop
-  case abs ih => 
-    constructor
-    intros x mem
-    exact ih x mem (by aesop)
-
-/-- Weakening of a typing derivation with an appended context. -/
-lemma weakening_strengthened : 
-    Γ ++ Δ ⊢ t ∶ τ → (Γ ++ Φ ++ Δ).Ok → (Γ ++ Φ ++ Δ) ⊢ t ∶ τ := by
-  generalize eq : Γ ++ Δ = Γ_Δ
-  intros h
-  revert Γ Δ Φ
-  induction h <;> intros Γ Δ Φ eq ok_Γ_Φ_Δ
-  case var => aesop
-  case app => aesop
-  case abs σ Γ' τ t xs ext ih =>
-    apply Typing.abs (xs ∪ (Γ ++ Φ ++ Δ).dom)
-    intros x _
-    have h : (x, σ) :: Γ ++ Δ = (x, σ) :: Γ' := by aesop
-    refine @ih x (by aesop) _ _ Φ h ?_
-    constructor <;> aesop
-
-/-- Weakening of a typing derivation by an additional context. -/
-lemma weakening : Γ ⊢ t ∶ τ → (Γ ++ Δ).Ok → Γ ++ Δ ⊢ t ∶ τ := by
-  intros der ok
-  rw [←List.append_nil (Γ ++ Δ)] at *
-  exact weakening_strengthened (by simp_all) ok
-
-/-- Typing derivations exist only for locally closed terms. -/
-lemma lc : Γ ⊢ t ∶ τ → t.LC := by
-  intros h
-  induction h <;> constructor
-  case abs ih => exact ih
-  all_goals aesop
-
-variable [HasFresh Var]
-
-open Term
-
-/-- Substitution for a context weakened by a single type between appended contexts. -/
-lemma subst_strengthened :
-    (Δ ++ (x, σ) :: Γ) ⊢ t ∶ τ →
-    Γ ⊢ s ∶ σ → 
-    (Δ ++ Γ) ⊢ (t [x := s]) ∶ τ := by
-  generalize  eq : Δ ++ (x, σ) :: Γ = Φ
-  intros h
-  revert Γ Δ
-  induction h <;> intros Γ Δ eq der
-  case app => aesop
-  case var x' τ ok mem => 
-    simp only [subst_fvar]
-    rw [←eq] at mem
-    rw [←eq] at ok
-    cases (Ctx.perm (by aesop) ok : @Ctx.Ok Var Ty _ ((x, σ) :: Δ ++ Γ))
-    case cons ok_weak _ =>
-    observe perm : (Γ ++ Δ).Perm (Δ ++ Γ)
-    by_cases h : x = x' <;> simp only [h]
-    · rw [show τ = σ by aesop]
-      refine Typing.perm perm (weakening der ?_)
-      exact Ctx.perm (id (List.Perm.symm perm)) ok_weak
-    · aesop
-  case abs σ Γ' t T2 xs ih' ih =>
-    apply Typing.abs (xs ∪ {x} ∪ (Δ ++ Γ).dom)
-    intros x _
-    rw [
-      subst_def, 
-      subst_open_var _ _ _ _ _ der.lc,
-      show (x, σ) :: (Δ ++ Γ) = ((x, σ) :: Δ) ++ Γ by aesop
-      ]
-    apply ih 
-    all_goals aesop  
-
-/-- Substitution for a context weakened by a single type. -/
-lemma typing_subst : 
-    (x, σ) :: Γ ⊢ t ∶ τ  → Γ ⊢ s ∶ σ → Γ ⊢ (t [x := s]) ∶ τ := by
-  intros weak der
-  rw [←List.nil_append Γ]
-  exact subst_strengthened weak der
-
-/-- Typing preservation for opening. -/
-@[aesop safe forward (rule_sets := [LambdaCalculus.LocallyNameless.ruleSet])]
-theorem preservation_opening {xs : Finset Var} :
-  (∀ x ∉ xs, (x, σ) :: Γ ⊢ m ^ fvar x ∶ τ) → 
-  Γ ⊢ n ∶ σ → Γ ⊢ m ^ n ∶ τ
-  := by
-  intros mem der
-  have ⟨fresh, free⟩ := fresh_exists (xs ∪ m.fv)
-  rw [subst_intro fresh n m (by aesop) der.lc]
-  refine typing_subst (mem fresh (by aesop)) der
-
-end STLC.Typing
-
-namespace Term.FullBeta
+namespace LambdaCalculus.LocallyNameless
 
 open STLC Typing
 
+variable {Var : Type u} [DecidableEq Var] {R : Term Var → Term Var → Prop}
+
+def preserves (R : Term Var → Term Var → Prop) := ∀ {Γ t t' τ}, Γ ⊢ t ∶ τ → R t t' → Γ ⊢ t' ∶ τ
+
+/-- If a reduction preserves types, so does its reflexive transitive closure. -/
+@[aesop safe forward (rule_sets := [LambdaCalculus.LocallyNameless.ruleSet])]
+theorem preservation_redex : preserves R → preserves (Relation.ReflTransGen R) := by
+  intros _ _ _ _ _ _ redex
+  induction redex <;> aesop
+
+open Relation in
+/-- Confluence preserves type preservation. -/
+theorem preservation_confluence : 
+    Confluence R → preserves R → Γ ⊢ a ∶ τ → 
+    (ReflTransGen R) a b → (ReflTransGen R) a c →
+    ∃ d, (ReflTransGen R) b d ∧ (ReflTransGen R) c d ∧ Γ ⊢ d ∶ τ := by
+  intros con p der ab ac
+  have ⟨d, bd, cd⟩ := con ab ac
+  exact ⟨d, bd, cd, preservation_redex p der (ab.trans bd)⟩
+ 
 variable [HasFresh Var] {Γ : Ctx Var Ty}
+
+namespace Term.FullBeta
 
 /-- Typing preservation for full beta reduction. -/
 @[aesop safe forward (rule_sets := [LambdaCalculus.LocallyNameless.ruleSet])]
@@ -144,22 +61,6 @@ theorem preservation : Γ ⊢ t ∶ τ → (t ⭢βᶠt') → Γ ⊢ t' ∶ τ :
   case' abs.abs xs _ _ _ xs' _=> apply Typing.abs (xs ∪ xs')
   case' app.beta der_l _ _ => cases der_l
   all_goals aesop
-
-/-- Typing preservation for multiple steps of full beta reduction. -/
-theorem preservation_redex : Γ ⊢ t ∶ τ → (t ↠βᶠ t') → Γ ⊢ t' ∶ τ := by
-  intros der redex
-  induction redex using Relation.ReflTransGen.trans_induction_on <;> aesop
-
-/-- Typing preservation for full beta confluence. -/
-theorem preservation_confluence :
-    Γ ⊢ a ∶ τ → (a ↠βᶠ b) → (a ↠βᶠ c) → 
-    ∃ d, (b ↠βᶠ d) ∧ (c ↠βᶠ d) ∧ Γ ⊢ d ∶ τ := by
-  intros der ab ac
-  have ⟨d, bd, cd⟩ := confluence_beta ab ac
-  refine ⟨d, bd, cd, preservation_redex der ?_⟩
-  trans
-  exact ab
-  exact bd
 
 omit [HasFresh Var] in
 /-- A typed term either full beta reduces or is a value. -/
