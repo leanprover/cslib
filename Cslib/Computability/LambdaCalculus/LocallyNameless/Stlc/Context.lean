bb502ec2a880ee4e5e94df836d1869d60e2dc2b0
Chris Henson <chrishenson.net@gmail.com>
2025-07-28 13:39:10 -0400

    use HasWellFormed notation

--- Cslib/Computability/LambdaCalculus/LocallyNameless/Stlc/Context.lean
+++ Cslib/Computability/LambdaCalculus/LocallyNameless/Stlc/Context.lean
@@ -5,6 +5,7 @@ Authors: Chris Henson
 -/
 
 import Cslib.Computability.LambdaCalculus.LocallyNameless.Untyped.AesopRuleset
+import Cslib.Syntax.HasWellFormed
 import Mathlib.Data.Finset.Defs
 import Mathlib.Data.Finset.Dedup
 import Mathlib.Data.List.Sigma
@@ -33,6 +34,9 @@ def dom : Ctx Var Ty → Finset Var := List.toFinset ∘ List.keys
 /-- A well-formed context. -/
 abbrev Ok : Ctx Var Ty → Prop := List.NodupKeys
 
+instance : HasWellFormed (Ctx Var Ty) :=
+  ⟨Ok⟩
+
 variable {Γ Δ : Ctx Var Ty}
 
 /-- Context membership is preserved on permuting a context. -/
@@ -43,11 +47,11 @@ theorem dom_perm_mem_iff (h : Γ.Perm Δ) {x : Var} :
 omit [DecidableEq Var] in
 /-- Context well-formedness is preserved on permuting a context. -/
 @[aesop safe forward (rule_sets := [LambdaCalculus.LocallyNameless.ruleSet])]
-theorem perm (h : Γ.Perm Δ) : Ok Γ → Ok Δ := (List.perm_nodupKeys h).mp
+theorem perm (h : Γ.Perm Δ) : Γ ✓ → Δ ✓ := (List.perm_nodupKeys h).mp
 
 omit [DecidableEq Var] in
 @[aesop safe forward (rule_sets := [LambdaCalculus.LocallyNameless.ruleSet])]
-theorem strengthen : (Δ ++ ⟨x, σ⟩ :: Γ).Ok → (Δ ++ Γ).Ok := by
+theorem strengthen : (Δ ++ ⟨x, σ⟩ :: Γ) ✓ → (Δ ++ Γ) ✓ := by
   intros ok
   have sl : List.Sublist (Δ ++ Γ) (Δ ++ ⟨x, σ⟩ :: Γ) := by simp
   exact List.NodupKeys.sublist sl ok
