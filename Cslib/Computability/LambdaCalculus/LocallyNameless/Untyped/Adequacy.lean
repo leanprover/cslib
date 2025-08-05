65d549c7b159a937346ca06f8cdbbefd2bfd7ef6
Fabrizio Montesi <famontesi@gmail.com>
2025-08-05 11:58:07 +0200

    Adequacy++

--- Cslib/Computability/LambdaCalculus/LocallyNameless/Untyped/Adequacy.lean
+++ Cslib/Computability/LambdaCalculus/LocallyNameless/Untyped/Adequacy.lean
@@ -13,7 +13,32 @@ variable {Var : Type u} [DecidableEq Var]
 
 namespace LambdaCalculus
 
+-- This seems obvious.
 def Named.Term.toLocallyNameless (m : Named.Term Var) : LocallyNameless.Term Var := sorry
 
-theorem alphaEqv_iff_eq (m n : Named.Term Var) : m =α n ↔ m.toLocallyNameless = n.toLocallyNameless := by
+-- This requires nontrivial design choices.
+-- def LocallyNameless.Term.toNamed (m : LocallyNameless.Term Var) : Named.Term Var := sorry
+
+section AlphaEquiv
+
+/-- Two named terms are α-equivalent iff their locally nameless variants are equal. -/
+theorem named_locallyNameless_alphaEquiv_iff_eq (m n : Named.Term Var) :
+  m =α n ↔ m.toLocallyNameless = n.toLocallyNameless := by
   sorry
+
+/-- Two locally-closed nameless terms are equal iff they are the nameless translation of
+α-equivalent named terms. -/
+theorem locallyNameless_lc_named_eq_iff_alphaEquiv (m n : LocallyNameless.Term Var) (hmlc : m.LC) (hnlc : n.LC) :
+  m = n ↔ (∃ m' n' : Named.Term Var,
+    m = m'.toLocallyNameless ∧ n = n'.toLocallyNameless ∧
+    m' =α n') := by
+  sorry
+
+end AlphaEquiv
+
+section Semantics
+
+/- Correspondences for the reduction systems.
+Could use (strong) bisimulation or even an isomorphism between the reduction systems. -/
+
+end Semantics
