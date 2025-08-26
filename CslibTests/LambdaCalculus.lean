507471d1c61f80fdac271f1227a28f12da98b981
Chris Henson <46805207+chenson2018@users.noreply.github.com>
2025-08-26 01:30:19 -0400

    Add additional test

--- CslibTests/LambdaCalculus.lean
+++ CslibTests/LambdaCalculus.lean
@@ -18,7 +18,19 @@ example : (abs 0 (var 0)) =α (abs 1 (var 1)) := by
   simp [Term.fv]
 
 example : (abs 1 (var 0)).subst 0 (app (var 1) (var 2)) = (abs 3 (app (var 1) (var 2))) := by
-  simp [Term.subst, Term.fv, Term.bv, Term.vars, Term.rename]
-  apply rfl
+  simp [subst, fv, bv, vars, rename, instHasFreshNat, HasFresh.ofSucc]
+
+def x := 0
+def y := 1
+def z := 2
+def w := 3
+
+attribute [simp] x y z w
+
+local instance coeNatTerm : Coe ℕ (Term ℕ) := ⟨Term.var⟩
+
+-- section 5.3.4 of TAPL
+example : (abs y (app x y))[x := (app y z : Term ℕ)] = (abs w (app (app y z) w)) := by 
+  simp [subst, fv, bv, vars, rename, instHasFreshNat, HasFresh.ofSucc, instHasSubstitutionTerm]
 
 -- example : (abs 0 (abs 1 (app (var 0) (var 1)))) =α (abs 1 (abs 0 (app (var 1) (var 0)))) := by
