2528b1b2bc1beb71a632c5f169724ee9732790f5
Yichen Xu <yichen.x@outlook.com>
2025-09-14 14:05:11 +0200

    Use wildcards for ununsed names

--- Cslib/Computability/LambdaCalculus/WellScoped/FSub/TypeSoundness/Preservation.lean
+++ Cslib/Computability/LambdaCalculus/WellScoped/FSub/TypeSoundness/Preservation.lean
@@ -32,28 +32,28 @@ theorem preservation
   HasType .empty t' T := by
   induction hred generalizing T
   case red_app_fun ih =>
-    have ⟨T1, T2, ht1, ht2, hs0⟩ := HasType.app_inv ht
+    have ⟨_, _, ht1, ht2, hs0⟩ := HasType.app_inv ht
     apply HasType.sub hs0
     apply HasType.app (ih ht1) ht2
   case red_app_arg ih =>
-    have ⟨T1, T2, ht1, ht2, hs0⟩ := HasType.app_inv ht
+    have ⟨_, _, ht1, ht2, hs0⟩ := HasType.app_inv ht
     apply HasType.sub hs0
     apply HasType.app ht1 (ih ht2)
   case red_app =>
-    have ⟨T1, T2, ht1, ht2, hs0⟩ := HasType.app_inv ht
-    have ⟨U0, h1, h2, h3⟩  := HasType.abs_inv ht1
+    have ⟨_, _, ht1, ht2, hs0⟩ := HasType.app_inv ht
+    have ⟨_, h1, h2, h3⟩  := HasType.abs_inv ht1
     have ht2' := HasType.sub h2 ht2
     have h1' := h1.retype (Retype.open_var ht2')
     simp only [Ty.rename_succVar_open_var] at h1'
     apply HasType.sub _ h1'
     apply Subtyp.trans h3 hs0
   case red_tapp_fun ih =>
-    have ⟨T2, ht1, hs2⟩ := HasType.tapp_inv ht
+    have ⟨_, ht1, hs2⟩ := HasType.tapp_inv ht
     apply HasType.sub hs2
     apply HasType.tapp (ih ht1)
   case red_tapp =>
-    have ⟨T2, ht1, hs2⟩ := HasType.tapp_inv ht
-    have ⟨U0, h1, h2, h3⟩ := HasType.tabs_inv ht1
+    have ⟨_, ht1, hs2⟩ := HasType.tapp_inv ht
+    have ⟨_, h1, h2, h3⟩ := HasType.tabs_inv ht1
     have h1' := h1.retype (Retype.open_tvar h2)
     have h3' := h3.retype (Retype.open_tvar Subtyp.refl)
     apply HasType.sub _ h1'
