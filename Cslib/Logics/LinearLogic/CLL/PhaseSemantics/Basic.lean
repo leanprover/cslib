9ef6bcb3e45299ceb62fd0da03d86dd18e2cec12
Tanner Duve <tannerduve@gmail.com>
2025-09-28 11:48:51 -0700

    progress on integrating

--- Cslib/Logics/LinearLogic/CLL/PhaseSemantics/Basic.lean
+++ Cslib/Logics/LinearLogic/CLL/PhaseSemantics/Basic.lean
@@ -1,7 +1,7 @@
 /-
 Copyright (c) 2025 Tanner Duve. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
-Authors: Tanner Duve
+Authors: Tanner Duve, Bhavik Mehta
 -/
 import Mathlib.Algebra.Group.Defs
 import Mathlib.Algebra.Group.Pointwise.Set.Basic
@@ -69,6 +69,10 @@ class PhaseSpace (M : Type u) extends CommMonoid M where
 
 namespace PhaseSpace
 
+variable {P : Type*} [PhaseSpace P] {p q : P}
+
+section
+
 -- ## Basic operations
 
 /--
@@ -117,6 +121,7 @@ lemma triple_orth [PhaseSpace M] (X : Set M) : X⫠⫠⫠ = X⫠ := by
     exact hm x hx'
   · apply orthogonal_extensive (X := X⫠)
 
+lemma triple_dual {G : Set P} : G⫠⫠⫠⫠ = G⫠⫠ := triple_orth G⫠
 /--
 The biorthogonal closure operator on sets in a phase space.
 -/
@@ -171,24 +176,45 @@ def isFact [PhaseSpace M] (X : Set M) : Prop := X = X⫠⫠
 /--
 The type of facts in a phase space.
 -/
-abbrev Fact (M : Type u) [PhaseSpace M] := { X : Set M // isFact X }
+structure Fact (P : Type*) [PhaseSpace P] where
+  (carrier : Set P)
+  (property : isFact carrier)
 
-instance [PhaseSpace M] : Coe (Fact M) (Set M) := ⟨Subtype.val⟩
+instance : SetLike (Fact P) P where
+  coe := Fact.carrier
+  coe_injective' := fun p q h => by cases p; cases q; congr
 
-lemma coe_mk [PhaseSpace M] {X : Set M} {h : isFact X} :
-    ((⟨X, h⟩ : Fact M) : Set M) = X := rfl
+instance [PhaseSpace M] : Coe (Fact M) (Set M) := ⟨Fact.carrier⟩
 
-@[simp] lemma closed [PhaseSpace M] (F : Fact M) : isFact (F : Set M) := F.property
+initialize_simps_projections Fact (carrier → coe)
 
-@[simp] lemma top_isFact [PhaseSpace M] :
-    isFact (univ : Set M) := by
+@[simp] lemma mem_carrier (G : Fact P) : G.carrier = (G : Set P) := rfl
+
+lemma subset_dual_dual (G : Set P) : G ⊆ G⫠⫠ := fun p hp q hq => mul_comm p q ▸ hq _ hp
+
+@[simps] def Fact.mk_subset (G : Set P) (h : G⫠⫠ ⊆ G) : Fact P where
+  carrier := G
+  property := by simp only [isFact]; symm; apply h.antisymm (subset_dual_dual G)
+
+lemma dual_subset_dual {G H : Set P} (h : G ⊆ H) : H⫠ ⊆ G⫠ := fun _ hp _ hq => hp _ (h hq)
+
+@[simps!] def Fact.mk_dual (G H : Set P) (h : G = H⫠) : Fact P :=
+  Fact.mk_subset G <| by rw [h, triple_orth]
+
+lemma coe_mk {X : Set P} {h : isFact X} :
+    ((⟨X, h⟩ : Fact P) : Set P) = X := rfl
+
+@[simp] lemma closed (F : Fact P) : isFact (F : Set P) := F.property
+
+@[simp] lemma top_isFact :
+    isFact (univ : Set P) := by
   rw [isFact]; symm
   simpa only [top_eq_univ]
-    using ClosureOperator.closure_top (CLL.PhaseSpace.biorthogonalClosure (M:=M))
+    using ClosureOperator.closure_top (CLL.PhaseSpace.biorthogonalClosure (M:=P))
 
 /-- In any phase space, `{1}⫠ = ⊥`. -/
-lemma orth_one_eq_bot [PhaseSpace M] :
-    ({(1 : M)} : Set M)⫠ = (PhaseSpace.bot : Set M) := by
+lemma orth_one_eq_bot :
+    ({(1 : P)} : Set P)⫠ = (PhaseSpace.bot : Set P) := by
   ext m; constructor
   · intro hm
     simpa [orthogonal, imp, mem_setOf, mul_one] using hm 1 (by simp)
@@ -197,14 +223,14 @@ lemma orth_one_eq_bot [PhaseSpace M] :
     simpa [orthogonal, imp, mem_setOf, mul_one] using hm
 
 /-- `0 := ⊤⫠` is a fact and is the smallest fact. -/
-@[simp] lemma zero_isFact [PhaseSpace M] : isFact ((∅ : Set M)⫠⫠) := by
+@[simp] lemma zero_isFact : isFact ((∅ : Set P)⫠⫠) := by
   simp only [isFact, triple_orth]
 
 /--
 A set is a fact if and only if it is the orthogonal of some set
 -/
-lemma fact_iff_exists_orth [PhaseSpace M] (X : Set M) :
-    isFact X ↔ ∃ Y : Set M, X = Y⫠ := by
+lemma fact_iff_exists_orth (X : Set P) :
+    isFact X ↔ ∃ Y : Set P, X = Y⫠ := by
   constructor
   · intro hX
     refine ⟨X⫠, ?_⟩
@@ -212,29 +238,32 @@ lemma fact_iff_exists_orth [PhaseSpace M] (X : Set M) :
   · rintro ⟨Y, rfl⟩
     simp only [isFact, triple_orth (X := Y)]
 
+/-- The fact given by the dual of G. -/
+@[simps!] def dualFact (G : Set P) : Fact P := Fact.mk_dual (G⫠) G rfl
+
 /-- `⊥` is a fact. -/
-@[simp] lemma bot_isFact [PhaseSpace M] : isFact (PhaseSpace.bot : Set M) := by
-  refine (fact_iff_exists_orth (M := M) (X := (PhaseSpace.bot : Set M))).2 ?_
-  exact ⟨{(1 : M)}, (orth_one_eq_bot (M := M)).symm⟩
+@[simp] lemma bot_isFact : isFact (PhaseSpace.bot : Set P) := by
+  refine (fact_iff_exists_orth (X := (PhaseSpace.bot : Set P))).2 ?_
+  exact ⟨{(1 : P)}, (orth_one_eq_bot).symm⟩
 
 /--
 The interpretation of the multiplicative unit 1: the biorthogonal closure of {1}.
 -/
-def oneSet [PhaseSpace M] : Set M := ({1} : Set M)⫠⫠
+def oneSet [PhaseSpace P] : Set P := ({1} : Set P)⫠⫠
 
-@[simp] lemma oneSet_isFact [PhaseSpace M] : isFact (oneSet : Set M) := by
+@[simp] lemma oneSet_isFact : isFact (oneSet : Set P) := by
   simp only [oneSet, isFact, triple_orth]
 
 /--
 The dual of oneSet is a fact.
 -/
-lemma oneSet_dual_isFact [PhaseSpace M] : isFact (oneSet⫠ : Set M) := by
+lemma oneSet_dual_isFact : isFact (oneSet⫠ : Set P) := by
   simp only [oneSet, isFact, triple_orth]
 
 /--
 If Y is a fact, then X ⊸ Y is also a fact
 -/
-lemma imp_isFact_of_fact [PhaseSpace M] (X Y : Set M) (hY : isFact Y) :
+lemma imp_isFact_of_fact (X Y : Set P) (hY : isFact Y) :
     isFact (X ⊸ Y) := by
   have hXY : (X ⊸ Y) = (X * Y⫠)⫠ := by
     ext m
@@ -253,11 +282,11 @@ lemma imp_isFact_of_fact [PhaseSpace M] (X Y : Set M) (hY : isFact Y) :
   simp only [isFact, hXY, triple_orth]
 
 /-- In a phase space, `G⫠⫠` is the smallest fact containing `G`. -/
-lemma biorth_least_fact [PhaseSpace M] (G : Set M) :
-      ∀ {F : Set M}, isFact F → G ⊆ F → G⫠⫠ ⊆ F := by
-  let c : ClosureOperator (Set M) := biorthogonalClosure
+lemma biorth_least_fact (G : Set P) :
+      ∀ {F : Set P}, isFact F → G ⊆ F → G⫠⫠ ⊆ F := by
+  let c : ClosureOperator (Set P) := biorthogonalClosure
   have h_min :
-      ∀ {F : Set M}, isFact F → G ⊆ F → G⫠⫠ ⊆ F := by
+      ∀ {F : Set P}, isFact F → G ⊆ F → G⫠⫠ ⊆ F := by
     intro F hF hGF
     have hF_closed : c.IsClosed F := by
       have : F = c F := by simpa [isFact, c] using hF
@@ -266,32 +295,32 @@ lemma biorth_least_fact [PhaseSpace M] (G : Set M) :
   apply h_min
 
 /-- `0` is the least fact (w.r.t. inclusion). -/
-lemma zero_least_fact [PhaseSpace M] :
-    ∀ {F : Set M}, isFact F → ((∅ : Set M)⫠⫠) ⊆ F := by
+lemma zero_least_fact :
+    ∀ {F : Set P}, isFact F → ((∅ : Set P)⫠⫠) ⊆ F := by
   intro F hF
-  have h := biorth_least_fact (M := M) (G := (∅ : Set M)) (F := F) hF
+  have h := biorth_least_fact (G := (∅ : Set P)) (F := F) hF
               (by simp)
   simpa using h
 
 /-- `⊤ = ∅⫠`, so `⊤` is a fact. -/
-@[simp] lemma top_eq_orth_empty [PhaseSpace M] :
-  (Set.univ : Set M) = (∅ : Set M)⫠ := by
+@[simp] lemma top_eq_orth_empty :
+  (Set.univ : Set P) = (∅ : Set P)⫠ := by
   ext m; simp [orthogonal, imp]
 
 /--
 Linear implication between a set and a fact, yielding a fact.
 -/
-def Fact.imp [PhaseSpace M] (X : Set M) (Y : Fact M) : Fact M :=
+def Fact.imp (X : Set P) (Y : Fact P) : Fact P :=
   ⟨ X ⊸ Y, imp_isFact_of_fact X Y Y.property ⟩
 
-lemma isFact_iff_closed [PhaseSpace M] (X : Set M) :
+lemma isFact_iff_closed (X : Set P) :
   isFact X ↔ biorthogonalClosure.IsClosed X := by
   constructor <;> (intro; simp only [isFact, biorthogonalClosure]; symm; assumption)
 
 /--
 Arbitrary intersections of facts are facts.
 -/
-lemma sInf_isFact [PhaseSpace M] {S : Set (Set M)}
+lemma sInf_isFact {S : Set (Set P)}
     (H : ∀ X ∈ S, isFact X) : isFact (sInf S) := by
   have H' : ∀ X ∈ S, biorthogonalClosure.IsClosed X :=
     fun X hX => (isFact_iff_closed (X := X)).1 (H X hX)
@@ -302,36 +331,45 @@ lemma sInf_isFact [PhaseSpace M] {S : Set (Set M)}
 /--
 Binary intersections of facts are facts.
 -/
-lemma inter_isFact_of_isFact [PhaseSpace M] {A B : Set M}
+lemma inter_isFact_of_isFact {A B : Set P}
     (hA : isFact A) (hB : isFact B) : isFact (A ∩ B) := by
-  have : isFact (sInf ({A,B} : Set (Set M))) := sInf_isFact (by
+  have : isFact (sInf ({A,B} : Set (Set P))) := sInf_isFact (by
     intro X hX; rcases hX with rfl | rfl | _; simp [hA]; simp [hB])
   simpa [sInf_insert, sInf_singleton, inf_eq_inter] using this
 
+-- instance : InfSet (Fact P) where
+--   sInf S := @sInf_isFact P _ S
+
+-- @[simp] lemma coe_sInf {S : Set (Fact P)} : ((sInf S : Fact P) : Set P) = ⋂ i ∈ S, i := rfl
+
+-- instance : Min (Fact P) where
+--   min G H := Fact.mk_dual (G ∩ H) (dual G ∪ dual H) <| by simp
+
 
 /-- `𝟭 := {1}⫠⫠ = ⊥⫠` -/
-lemma oneSet_eq_bot_orth [PhaseSpace M] :
-    (oneSet : Set M) = (PhaseSpace.bot : Set M)⫠ := by
+lemma oneSet_eq_bot_orth :
+    (oneSet : Set P) = (PhaseSpace.bot : Set P)⫠ := by
   simp only [oneSet, orth_one_eq_bot]
 
 /-- for any fact `G`, we have `𝟭 · G = G` -/
-lemma one_mul_fact_set [PhaseSpace M] (G : Fact M) :
-    (oneSet : Set M) * (G : Set M) = (G : Set M) := by
+lemma one_mul_fact_set (G : Fact P) :
+    (oneSet : Set P) * (G : Set P) = (G : Set P) := by
   apply le_antisymm
   · intro z hz
     rcases hz with ⟨a, ha, q, hq, rfl⟩
-    have : a * q ∈ ((G : Set M)⫠⫠) := by
+    have : a * q ∈ ((G : Set P)⫠⫠) := by
       intro y hy
-      have hyq : y * q ∈ (PhaseSpace.bot : Set M) := by
+      have hyq : y * q ∈ (PhaseSpace.bot : Set P) := by
         simpa [orthogonal, imp, Set.mem_setOf] using hy q hq
-      have ha' : a ∈ (PhaseSpace.bot : Set M)⫠ := by
+      have ha' : a ∈ (PhaseSpace.bot : Set P)⫠ := by
         simpa [oneSet_eq_bot_orth] using ha
       have : a * (y * q) ∈ PhaseSpace.bot := ha' _ hyq
       simpa [mul_left_comm, mul_comm, mul_assoc] using this
-    rw [G.property]; exact this
+    have h : isFact (G : Set P) := G.property
+    rw [h]; exact this
   · intro g hg
-    have h1 : (1 : M) ∈ (oneSet : Set M) := by
-      have : (1 : M) ∈ ({(1 : M)} : Set M) := by simp
+    have h1 : (1 : P) ∈ (oneSet : Set P) := by
+      have : (1 : P) ∈ ({(1 : P)} : Set P) := by simp
       exact orthogonal_extensive _ this
     exact ⟨1, h1, g, hg, by simp⟩
 
@@ -356,29 +394,29 @@ inductive unop where
 inductive binop where
 | tensor | parr | with | oplus
 
-inductive FactExpr [PhaseSpace M] where
+inductive FactExpr where
 | const (c : const) : FactExpr
-| unop (u : unop) (X : Fact M) : FactExpr
-| binop (b : binop) (X Y : Fact M) : FactExpr
+| unop (u : unop) (X : Fact P) : FactExpr
+| binop (b : binop) (X Y : Fact P) : FactExpr
 
-def constInterpret [PhaseSpace M] (c : const) : Fact M :=
+def constInterpret (c : const) : Fact P :=
 match c with
 | .one => ⟨oneSet, oneSet_isFact⟩
-| .zero => ⟨(∅ : Set M)⫠⫠, zero_isFact⟩
-| .top => ⟨(Set.univ : Set M), top_isFact⟩
+| .zero => ⟨(∅ : Set P)⫠⫠, zero_isFact⟩
+| .top => ⟨(Set.univ : Set P), top_isFact⟩
 | .bot => ⟨oneSet⫠, oneSet_dual_isFact⟩
 
-def unopInterpret [PhaseSpace M] (u : unop) (X : Fact M) : Fact M := match u with
-| .bang => ⟨(X ∩ I)⫠⫠, by rw [fact_iff_exists_orth]; use (X ∩ I)⫠⟩
-| .quest => ⟨(X⫠ ∩ I)⫠, by rw [fact_iff_exists_orth]; use (X⫠ ∩ I)⟩
+def unopInterpret (u : unop) (X : Fact P) : Fact P := match u with
+| .bang => dualFact (X ∩ I)⫠
+| .quest => dualFact (X⫠ ∩ I)
 
-def binopInterpret [PhaseSpace M] (b : binop) (X Y : Fact M) : Fact M := match b with
-| .tensor => ⟨(X * Y)⫠⫠, by rw [fact_iff_exists_orth]; use (X * Y)⫠⟩
-| .parr => ⟨((X⫠) * (Y⫠))⫠, by rw [fact_iff_exists_orth]; use ((X⫠) * (Y⫠))⟩
-| .with => ⟨(X ∩ Y), by apply inter_isFact_of_isFact <;> aesop⟩
-| .oplus => ⟨(X ∪ Y)⫠⫠, by rw [fact_iff_exists_orth]; use (X ∪ Y)⫠⟩
+def binopInterpret (b : binop) (X Y : Fact P) : Fact P := match b with
+| .tensor => dualFact (X * Y)⫠
+| .parr => dualFact ((X⫠) * (Y⫠))
+| .with => sorry
+| .oplus => dualFact (X ∪ Y)⫠
 
-def interpret [PhaseSpace M] (c : @FactExpr M _) : Fact M := match c with
+def interpret (c : @FactExpr P _) : Fact P := match c with
 | .const c => constInterpret c
 | .unop u X => unopInterpret u X
 | .binop b X Y => binopInterpret b X Y
@@ -403,7 +441,3 @@ def interpProp [PhaseSpace M] (v : Atom → Fact M) : Proposition Atom → Fact 
   | .quest  A     => interpret (.unop  .quest  (interpProp v A))
 
 @[inherit_doc] scoped notation:max "⟦" P "⟧" v:90 => interpProp v P
-
-end PhaseSpace
-
-end CLL
