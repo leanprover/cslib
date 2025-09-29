b1e50147167df5d4e084f692d3031d4c2a5f7d6f
Fabrizio Montesi <famontesi@gmail.com>
2025-09-29 10:32:40 +0200

    feat: move general defs and theorems from DFA to DA

--- Cslib/Computability/Automata/DA.lean
+++ Cslib/Computability/Automata/DA.lean
@@ -4,6 +4,8 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Fabrizio Montesi
 -/
 
+import Cslib.Foundations.Semantics.LTS.Basic
+
 /-! # Deterministic Automaton
 
 A Deterministic Automaton (DA) is a deterministic labelled transition system with an initial state.
@@ -14,3 +16,57 @@ structure DA (State : Type _) (Symbol : Type _) where
   start : State
   /-- The transition function of the automaton. -/
   tr : State → Symbol → State
+
+namespace DA
+
+/-- Extended transition function.
+
+Implementation note: compared to [Hopcroft2006], the definition consumes the input list of symbols
+from the left (instead of the right), in order to follow the way lists are constructed.
+-/
+@[grind]
+def mtr (da : DA State Symbol) (s : State) (xs : List Symbol) :=
+  match xs with
+  | [] => s
+  | x :: xs => da.mtr (da.tr s x) xs
+
+@[grind]
+theorem mtr_nil_eq {da : DA State Symbol} : da.mtr s [] = s := by rfl
+
+section LTS
+
+/-- `DA` is a special case of `LTS`. -/
+@[grind]
+def toLTS (da : DA State Symbol) : LTS State Symbol :=
+  LTS.mk (fun s1 μ s2 => da.tr s1 μ = s2)
+
+instance : Coe (DA State Symbol) (LTS State Symbol) where
+  coe := toLTS
+
+/-- `DA.toLTS` correctly characterises transitions. -/
+@[grind]
+theorem toLTS_tr {da : DA State Symbol} :
+  da.toLTS.Tr s1 μ s2 ↔ da.tr s1 μ = s2 := by
+  rfl
+
+/-- `DA.toLTS` correctly characterises multistep transitions. -/
+@[grind]
+theorem toLTS_mtr {da : DA State Symbol} :
+  da.toLTS.MTr s1 xs s2 ↔ da.mtr s1 xs = s2 := by
+  constructor <;> intro h
+  case mp =>
+    induction h <;> grind
+  case mpr =>
+    induction xs generalizing s1
+    case nil => grind
+    case cons x xs ih =>
+      apply LTS.MTr.stepL (s2 := da.tr s1 x) <;> grind
+
+/-- The LTS induced by a DA is deterministic. -/
+@[grind]
+theorem toLTS_deterministic (da : DA State Symbol) : da.toLTS.Deterministic := by
+  grind
+
+end LTS
+
+end DA
--- Cslib/Computability/Automata/DFA/Basic.lean
+++ Cslib/Computability/Automata/DFA/Basic.lean
@@ -31,69 +31,23 @@ structure DFA (State : Type _) (Symbol : Type _) extends DA State Symbol where
 
 namespace DFA
 
-/-- Extended transition function.
-
-Implementation note: compared to [Hopcroft2006], the definition consumes the input list of symbols
-from the left (instead of the right), in order to follow the way lists are constructed.
--/
-@[grind]
-def mtr (dfa : DFA State Symbol) (s : State) (xs : List Symbol) :=
-  match xs with
-  | [] => s
-  | x :: xs => dfa.mtr (dfa.tr s x) xs
-
-@[grind]
-theorem mtr_nil_eq {dfa : DFA State Symbol} : dfa.mtr s [] = s := by rfl
-
-/-- A DFA accepts a trace if there is a multi-step accepting derivative with that trace from
+/-- A DA accepts a trace if there is a multi-step accepting derivative with that trace from
 the start state. -/
 @[grind]
 def Accepts (dfa : DFA State Symbol) (μs : List Symbol) :=
   (dfa.mtr dfa.start μs) ∈ dfa.accept
 
-/-- The language of a DFA is the set of traces that it accepts. -/
+/-- The language of a DA is the set of traces that it accepts. -/
 @[grind]
 def language (dfa : DFA State Symbol) : Set (List Symbol) :=
   { μs | dfa.Accepts μs }
 
-/-- A trace is accepted by a DFA iff it is in the language of the DFA. -/
+/-- A trace is accepted by a DA iff it is in the language of the DA. -/
 @[grind]
 theorem accepts_mem_language (dfa : DFA State Symbol) (μs : List Symbol) :
   dfa.Accepts μs ↔ μs ∈ dfa.language := by rfl
 
 section LTS
-
-/-- `DFA` is a special case of `LTS`. -/
-@[grind]
-def toLTS (dfa : DFA State Symbol) : LTS State Symbol :=
-  LTS.mk (fun s1 μ s2 => dfa.tr s1 μ = s2)
-
-instance : Coe (DFA State Symbol) (LTS State Symbol) where
-  coe := toLTS
-
-/-- `DFA.toLTS` correctly characterises transitions. -/
-@[grind]
-theorem toLTS_tr {dfa : DFA State Symbol} :
-  dfa.toLTS.Tr s1 μ s2 ↔ dfa.tr s1 μ = s2 := by
-  rfl
-
-/-- `DFA.toLTS` correctly characterises multistep transitions. -/
-@[grind]
-theorem toLTS_mtr {dfa : DFA State Symbol} :
-  dfa.toLTS.MTr s1 xs s2 ↔ dfa.mtr s1 xs = s2 := by
-  constructor <;> intro h
-  case mp =>
-    induction h <;> grind
-  case mpr =>
-    induction xs generalizing s1
-    case nil => grind
-    case cons x xs ih =>
-      apply LTS.MTr.stepL (s2 := dfa.tr s1 x) <;> grind
-
-/-- The LTS induced by a DFA is deterministic. -/
-@[grind]
-theorem toLTS_deterministic (dfa : DFA State Symbol) : dfa.toLTS.Deterministic := by
-  grind
 
 /-- The LTS induced by a DFA is finite-state. -/
 @[grind]
