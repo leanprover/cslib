989d0bebfcd19883e8a56a852e68d5c2b6bd1249
twwar <tom.waring@unimelb.edu.au>
2025-10-10 13:24:22 +0200

    axioms to assumptions

--- Cslib/Logics/Propositional/NaturalDeduction/Lemmas.lean
+++ Cslib/Logics/Propositional/NaturalDeduction/Lemmas.lean
@@ -19,7 +19,7 @@ namespace NJ
 
 open Proposition Theory Derivation
 
-variable {Atom : Type _} [DecidableEq Atom] (T : Theory Atom)
+variable {Atom : Type _} [DecidableEq Atom] {T : Theory Atom}
 
 theorem Theory.le_wd {A A' B B' : Proposition Atom} (hA : A ≡[T] A') (hB : B ≡[T] B') :
     {A} ⊢[T] B ↔ {A'} ⊢[T] B' := by
@@ -120,6 +120,82 @@ theorem Theory.le_himp_iff {A B C : Proposition Atom} :
     rw [← show ({B,A} : Ctx Atom) ∪ ∅ = {B,A} by grind]
     refine Theory.Derivation.cut (Γ := {B,A}) (Δ := ∅) ?_ D
     apply conjI <;> exact ass <| by grind
+
+/-- Here we refer to the terminology of `Mathlib.Order.Heyting.Regular`. -/
+theorem Theory.regular [Bot Atom] [IsClassical T] {A : Proposition Atom} : A ≡[T] ~~A := by
+  constructor; constructor
+  · apply implI; apply implE (A := A) <;> exact ass <| by grind
+  · apply implE (A := ~~A)
+    · apply ax <| by grind
+    · apply ass <| by grind
+
+/-! ### Converting between assumptions and axioms -/
+
+def Theory.Derivation.axsToAss {T : Theory Atom} {Γ Δ : Ctx Atom} {B : Proposition Atom} :
+    (T ∪ Δ).Derivation ⟨Γ, B⟩ → T.Derivation ⟨Γ ∪ Δ, B⟩
+  | @ax _ _ _ _ B _ => by
+    by_cases B ∈ Δ
+    case pos => exact ass <| by grind
+    case neg => exact ax <| by grind
+  | ass _ => ass <| by grind
+  | conjI D E => conjI (axsToAss D) (axsToAss E)
+  | conjE₁ D => conjE₁ (axsToAss D)
+  | conjE₂ D => conjE₂ (axsToAss D)
+  | disjI₁ D => disjI₁ (axsToAss D)
+  | disjI₂ D => disjI₂ (axsToAss D)
+  | @disjE _ _ _ _ A' B C D E₁ E₂ => by
+    refine disjE (axsToAss D) ?_ ?_
+    · let E₁' : _ := axsToAss (B := C) E₁
+      rw [Finset.insert_union] at E₁'
+      assumption
+    · let E₂' : _ := axsToAss (B := C) E₂
+      rw [Finset.insert_union] at E₂'
+      assumption
+  | @implI _ _ _ A' B _ D => by
+    let D' : _ := axsToAss (B := B) D
+    rw [Finset.insert_union] at D'
+    exact implI _ D'
+  | implE D E => implE (axsToAss D) (axsToAss E)
+
+theorem Theory.SDerivable.axs_to_ass {T : Theory Atom} {Γ Δ : Ctx Atom} {B : Proposition Atom} :
+    Γ ⊢[T ∪ Δ] B → (Γ ∪ Δ) ⊢[T] B
+  | ⟨D⟩ => ⟨Theory.Derivation.axsToAss D⟩
+
+private lemma Finset.insert_sdiff_subset {α : Type _} [DecidableEq α] (x : α) (s t : Finset α) :
+    insert x s \ t ⊆ insert x (s \ t) := by grind
+
+private def Theory.Derivation.assToAxs' {T : Theory Atom} {Γ Δ : Ctx Atom} {B : Proposition Atom} :
+    T.Derivation ⟨Γ, B⟩ → (T ∪ Δ).Derivation ⟨Γ \ Δ, B⟩
+  | @ass _ _ _ _ B _ => by
+    by_cases B ∈ Δ
+    case pos =>
+      exact ax <| by grind
+    case neg =>
+      exact ass <| by grind
+  | ax _ => ax <| by grind
+  | conjI D E => conjI (assToAxs' D) (assToAxs' E)
+  | conjE₁ D => conjE₁ (assToAxs' D)
+  | conjE₂ D => conjE₂ (assToAxs' D)
+  | disjI₁ D => disjI₁ (assToAxs' D)
+  | disjI₂ D => disjI₂ (assToAxs' D)
+  | @disjE _ _ _ _ A' B C D E₁ E₂ =>
+    disjE (assToAxs' D)
+      ((assToAxs' (Δ := Δ) (B := C) E₁).weak_ctx <| Finset.insert_sdiff_subset A' Γ Δ)
+      ((assToAxs' (Δ := Δ) (B := C) E₂).weak_ctx <| Finset.insert_sdiff_subset B Γ Δ)
+  | @implI _ _ _ A' B _ D =>
+    implI _ ((assToAxs' (Δ := Δ) (B := B) D).weak_ctx <| Finset.insert_sdiff_subset A' Γ Δ)
+  | implE D E => implE (assToAxs' D) (assToAxs' E)
+
+def Theory.Derivation.assToAxs {T : Theory Atom} {Γ Δ : Ctx Atom} {B : Proposition Atom}
+    (D : T.Derivation ⟨Γ ∪ Δ, B⟩) : (T ∪ Δ).Derivation ⟨Γ, B⟩ := (assToAxs' D).weak_ctx <| by grind
+
+theorem Theory.SDerivable.ass_to_axs {T : Theory Atom} {Γ Δ : Ctx Atom} {B : Proposition Atom} :
+    (Γ ∪ Δ) ⊢[T] B → Γ ⊢[T ∪ Δ] B
+  | ⟨D⟩ => ⟨Theory.Derivation.assToAxs D⟩
+
+theorem Theory.SDerivable.iff_sDerivable_extension {Γ Δ : Ctx Atom} {B : Proposition Atom} :
+    (Γ ∪ Δ) ⊢[T] B ↔ Γ ⊢[T ∪ Δ] B := ⟨Theory.SDerivable.ass_to_axs, Theory.SDerivable.axs_to_ass⟩
+
 
 end NJ
 
