1156f01c7c405e89d38df61b6f23c0e0d6012ec0
Fabrizio Montesi <famontesi@gmail.com>
2025-10-10 09:24:52 +0200

    omega sequence with automatic coercions back and forth with functions

--- Cslib/Foundations/Data/OmegaSequence.lean
+++ Cslib/Foundations/Data/OmegaSequence.lean
@@ -5,17 +5,23 @@ Authors: Ching-Tsun Chou, Fabrizio Montesi
 -/
 
 import Mathlib.Data.Nat.Notation
+import Mathlib.Data.FunLike.Basic
 
 /-! # ω-sequences -/
 
 /-- An `ωSequence α` is an infinite sequence of elements of type `α`. Elements are accessed by means
 of function application. -/
-def ωSequence (α : Type _) := ℕ → α
+structure ωSequence (α : Type _) where
+  get : ℕ → α
 
-/-
-instance : GetElem (ωSequence α) ℕ α (fun _ _ => True) where
-  getElem s i := fun _ => s i
--/
+instance : FunLike (ωSequence α) ℕ α where
+  coe s := s.get
+  coe_injective' := by
+    rintro ⟨get1⟩ ⟨get2⟩
+    grind
+
+instance : Coe (ℕ → α) (ωSequence α) where
+  coe f := ⟨f⟩
 
 namespace ωSequence
 
@@ -25,7 +31,9 @@ def drop (n : ℕ) (s : ωSequence α) : ωSequence α := fun i => s (i + n)
 
 /-- Drops can be contracted by adding the numbers of dropped elements. -/
 theorem drop_add {s : ωSequence α} (n1 n2 : ℕ) : (s.drop n1).drop n2 = s.drop (n1 + n2) := by
-  funext i
+  simp only [drop, DFunLike.coe]
+  congr
+  ext i
   grind
 
 /-- Applies a function to each element of an ω-sequence, returning the resulting ω-sequence. -/
