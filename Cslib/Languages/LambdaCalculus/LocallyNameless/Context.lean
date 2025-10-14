ef380fb420ee227d3d241fb1a5bfed9bd5a057e3
Kim Morrison <kim@tqft.net>
2025-10-14 22:13:41 +1100

    update for changes in Mathlib

--- Cslib/Languages/LambdaCalculus/LocallyNameless/Context.lean
+++ Cslib/Languages/LambdaCalculus/LocallyNameless/Context.lean
@@ -19,29 +19,6 @@ universe u v
 
 variable {α : Type u} {β : Type v}
 
--- TODO: These are pieces of API that cannot be directly automated by adding `grind` attributes to
--- `Mathlib.Data.List.Sigma`. We should consider upstreaming them to Mathlib.
-namespace List
-
-variable {β : α → Type v} {l₁ l₂ : List (Sigma β)}
-
-/-- Keys distribute with appending. -/
-theorem keys_append : (l₁ ++ l₂).keys = l₁.keys ++ l₂.keys := by
-  simp [keys]
-
-variable [DecidableEq α] in
-/-- Sublists without duplicate keys preserve lookups. -/
-theorem sublist_dlookup (nd₂ : l₂.NodupKeys) (s : l₁ <+ l₂) (mem : b ∈ l₁.dlookup a) : 
-    b ∈ l₂.dlookup a := by
-  grind [Option.mem_def, → perm_nodupKeys, dlookup_append, => perm_dlookup,
-    → Sublist.exists_perm_append]
-
-@[grind =]
-lemma nodupKeys_middle : (l₁ ++ s :: l₂).NodupKeys ↔ (s :: (l₁ ++ l₂)).NodupKeys := by
-  simp_all [NodupKeys, keys, nodup_middle]
-
-end List
-
 namespace LambdaCalculus.LocallyNameless
 
 variable [DecidableEq α]
@@ -92,11 +69,11 @@ omit [DecidableEq α] in
 /-- Context well-formedness is preserved on removing an element. -/
 @[scoped grind →]
 theorem wf_strengthen (ok : (Δ ++ ⟨x, σ⟩ :: Γ)✓) : (Δ ++ Γ)✓ := by
-  grind [keys_append]
+  grind
 
 /-- A mapping of values within a context. -/
 @[simp, scoped grind]
-def map_val (f : β → β) (Γ : Context α β) : Context α β := 
+def map_val (f : β → β) (Γ : Context α β) : Context α β :=
   Γ.map (fun ⟨var,ty⟩ => ⟨var,f ty⟩)
 
 omit [DecidableEq α] in
