77acd3fbfcb579d9fa10c6af59c72fac49e7221f
Kim Morrison <kim@tqft.net>
2025-10-15 15:07:51 +1100

    chore: run 'lake exe mk_all'

---
+++ CslibTests.lean
@@ -0,0 +1,9 @@
+import CslibTests.Bisimulation
+import CslibTests.CCS
+import CslibTests.DFA
+import CslibTests.FreeMonad
+import CslibTests.HasFresh
+import CslibTests.LTS
+import CslibTests.LambdaCalculus
+import CslibTests.NamespaceCheck
+import CslibTests.ReductionSystem
