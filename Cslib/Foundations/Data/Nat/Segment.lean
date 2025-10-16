a42425f4ea2c147b4aef53466b330ee17cc69a50
Ching-Tsun Chou <chingtsun.chou@gmail.com>
2025-10-16 11:58:36 -0700

    Incorporate Chris Henson's comments on Cslib/Foundations/Data/Nat/Segment.lean

--- Cslib/Foundations/Data/Nat/Segment.lean
+++ Cslib/Foundations/Data/Nat/Segment.lean
@@ -8,8 +8,6 @@ import Mathlib.Algebra.Order.Sub.Basic
 import Mathlib.Data.Nat.Nth
 import Mathlib.Tactic
 
-namespace Cslib
-
 open Function Set
 
 /-!
@@ -18,6 +16,7 @@ Given a strictly monotonic function `φ : ℕ → ℕ` and `k : ℕ` with `k ≥
 `Nat.segment φ k` is defined to be 0 for `k < ϕ 0`.
 This file defines `Nat.segment` and proves various properties aboout it.
 -/
+@[scoped grind]
 noncomputable def Nat.segment (φ : ℕ → ℕ) (k : ℕ) : ℕ :=
   open scoped Classical in
   Nat.count (· ∈ range φ) (k + 1) - 1
@@ -26,21 +25,19 @@ variable {φ : ℕ → ℕ}
 
 /-- Any strictly monotonic function `φ : ℕ → ℕ` has an infinite range. -/
 theorem Nat.strict_mono_infinite (hm : StrictMono φ) :
-    (range φ).Infinite := by
-  exact infinite_range_of_injective hm.injective
+    (range φ).Infinite :=
+  infinite_range_of_injective hm.injective
 
 /-- Any infinite suset of `ℕ` is the range of a strictly monotonic function. -/
 theorem Nat.infinite_strict_mono {ns : Set ℕ} (h : ns.Infinite) :
-    ∃ φ : ℕ → ℕ, StrictMono φ ∧ range φ = ns := by
-  use Nat.nth (· ∈ ns) ; constructor
-  · exact Nat.nth_strictMono h
-  · exact Nat.range_nth_of_infinite h
+    ∃ φ : ℕ → ℕ, StrictMono φ ∧ range φ = ns :=
+  ⟨Nat.nth (· ∈ ns), Nat.nth_strictMono h, Nat.range_nth_of_infinite h⟩
 
-open scoped Classical in
 /-- There is a gap between two successive occurrences of a predicate `p : ℕ → Prop`,
 assuming `p` (as a set) is infinite. -/
 theorem Nat.nth_succ_gap {p : ℕ → Prop} (hf : (setOf p).Infinite) (n : ℕ) :
     ∀ k < Nat.nth p (n + 1) - Nat.nth p n, k > 0 → ¬ p (k + Nat.nth p n) := by
+  classical
   intro k h_k1 h_k0 h_p_k
   let m := Nat.count p (k + Nat.nth p n)
   have h_k_ex : Nat.nth p m = k + Nat.nth p n := by simp [m, Nat.nth_count h_p_k]
@@ -52,22 +49,15 @@ theorem Nat.nth_succ_gap {p : ℕ → Prop} (hf : (setOf p).Infinite) (n : ℕ) 
 element of the range of `φ`. -/
 theorem Nat.nth_of_strict_mono (hm : StrictMono φ) (n : ℕ) :
     φ n = Nat.nth (· ∈ range φ) n := by
-  rw [← Nat.nth_comp_of_strictMono hm (by simp)]
-  · simp
-  intro hf ; exfalso
-  have : (range φ).Infinite := strict_mono_infinite hm
-  exact absurd hf this
+  have (hf : (range φ).Finite) : False := hf.not_infinite (strict_mono_infinite hm)
+  rw [←Nat.nth_comp_of_strictMono hm] <;> first | grind | simp
 
 open scoped Classical in
 /-- If `φ 0 = 0`, then `0` is below any `n` not in the range of `φ`. -/
 theorem Nat.count_notin_range_pos (h0 : φ 0 = 0) (n : ℕ) (hn : n ∉ range φ) :
     Nat.count (· ∈ range φ) n > 0 := by
-  have h0' : 0 ∈ range φ := by use 0
-  have h1 : n ≠ 0 := by rintro ⟨rfl⟩ ; contradiction
-  have h2 : 1 ≤ n := by omega
-  have h3 := Nat.count_monotone (· ∈ range φ) h2
-  simp only [Nat.count_succ, Nat.count_zero, h0', ↓reduceIte, zero_add, gt_iff_lt] at h3 ⊢
-  omega
+  have := Nat.count_monotone (· ∈ range φ) (show 1 ≤ n by grind)
+  grind
 
 /-- For a strictly monotonic function `φ : ℕ → ℕ`, no number (strictly) between
 `φ m` and ` φ (m + 1)` is in the range of `φ`. -/
@@ -81,20 +71,20 @@ theorem Nat.strict_mono_range_gap (hm : StrictMono φ) {m k : ℕ}
   rw [(show k - Nat.nth (· ∈ range φ) m + Nat.nth (· ∈ range φ) m = k by omega)] at h_gap
   exact h_gap
 
-open scoped Classical in
 /-- For a strictly monotonic function `φ : ℕ → ℕ`, the segment of `φ k` is `k`. -/
+@[simp]
 theorem Nat.segment_idem (hm : StrictMono φ) (k : ℕ) :
     segment φ (φ k) = k := by
-  have h1 : Nat.count (· ∈ range φ) (φ k + 1) = Nat.count (· ∈ range φ) (φ k) + 1 := by
-    apply Nat.count_succ_eq_succ_count ; simp
-  rw [segment, h1, Nat.add_one_sub_one, nth_of_strict_mono hm]
-  have h_eq := Nat.count_nth_of_infinite (p := (· ∈ range φ)) <| strict_mono_infinite hm
-  rw [h_eq]
+  classical
+  have := Nat.count_nth_of_infinite (p := (· ∈ range φ)) <| strict_mono_infinite hm
+  have := nth_of_strict_mono hm
+  grind [segment]
 
-open scoped Classical in
 /-- For a strictly monotonic function `φ : ℕ → ℕ`, `segment φ k = 0` for all `k < φ 0`. -/
+@[scoped grind =]
 theorem Nat.segment_pre_zero (hm : StrictMono φ) {k : ℕ} (h : k < φ 0) :
     segment φ k = 0 := by
+  classical
   have h1 : Nat.count (· ∈ range φ) (k + 1) = 0 := by
     apply Nat.count_of_forall_not
     rintro n h_n ⟨i, rfl⟩
@@ -103,6 +93,7 @@ theorem Nat.segment_pre_zero (hm : StrictMono φ) {k : ℕ} (h : k < φ 0) :
   rw [segment, h1]
 
 /-- For a strictly monotonic function `φ : ℕ → ℕ` with `φ 0 = 0`, `segment φ 0 = 0`. -/
+@[scoped grind =]
 theorem Nat.segment_zero (hm : StrictMono φ) (h0 : φ 0 = 0) :
     segment φ 0 = 0 := by
   calc _ = segment φ (φ 0) := by simp [h0]
@@ -115,63 +106,51 @@ theorem Nat.segment_plus_one (h0 : φ 0 = 0) (k : ℕ) :
   suffices _ : Nat.count (· ∈ range φ) (k + 1) ≠ 0 by unfold segment ; omega
   apply Nat.count_ne_iff_exists.mpr ; use 0 ; grind
 
-open scoped Classical in
 /-- For a strictly monotonic function `φ : ℕ → ℕ` with `φ 0 = 0`,
 `k < φ (segment φ k + 1)` for all `k : ℕ`. -/
 theorem Nat.segment_upper_bound (hm : StrictMono φ) (h0 : φ 0 = 0) (k : ℕ) :
     k < φ (segment φ k + 1) := by
+  classical
   rw [nth_of_strict_mono hm (segment φ k + 1), segment_plus_one h0 k]
   suffices _ : k + 1 ≤ Nat.nth (· ∈ range φ) (Nat.count (· ∈ range φ) (k + 1)) by omega
   apply Nat.le_nth_count
   exact strict_mono_infinite hm
 
-open scoped Classical in
 /-- For a strictly monotonic function `φ : ℕ → ℕ` with `φ 0 = 0`,
 `φ (segment φ k) ≤ k` for all `k : ℕ`. -/
 theorem Nat.segment_lower_bound (hm : StrictMono φ) (h0 : φ 0 = 0) (k : ℕ) :
     φ (segment φ k) ≤ k := by
+  classical
   rw [nth_of_strict_mono hm (segment φ k), segment]
   rcases Classical.em (k ∈ range φ) with h_k | h_k
-  · have h1 : Nat.count (· ∈ range φ) (k + 1) = Nat.count (· ∈ range φ) k + 1 := by
-      exact Nat.count_succ_eq_succ_count h_k
-    rw [h1, Nat.add_one_sub_one, Nat.nth_count h_k]
-  · have h2 : Nat.count (· ∈ range φ) (k + 1) = Nat.count (· ∈ range φ) k := by
-      exact Nat.count_succ_eq_count h_k
+  · simp_all [Nat.count_succ_eq_succ_count]
+  · have h1 : Nat.count (· ∈ range φ) k > 0 := count_notin_range_pos h0 k h_k
+    have h2 : Nat.count (· ∈ range φ) (k + 1) = Nat.count (· ∈ range φ) k :=
+      Nat.count_succ_eq_count h_k
     rw [h2]
     suffices _ : Nat.nth (· ∈ range φ) (Nat.count (· ∈ range φ) k - 1) < k by omega
     apply Nat.nth_lt_of_lt_count
-    have : Nat.count (· ∈ range φ) k > 0 := by exact count_notin_range_pos h0 k h_k
     omega
 
-open scoped Classical in
 /-- For a strictly monotonic function `φ : ℕ → ℕ`, all `k` satisfying `φ m ≤ k < φ (m + 1)`
 has `segment φ k = m`. -/
 theorem Nat.segment_range_val (hm : StrictMono φ) {m k : ℕ}
     (hl : φ m ≤ k) (hu : k < φ (m + 1)) : segment φ k = m := by
+  classical
   obtain (rfl | hu') := show φ m = k ∨ φ m < k by omega
   · exact segment_idem hm m
-  obtain ⟨j, h_j, rfl⟩ : ∃ j < φ (m + 1) - φ m - 1, k = j + φ m + 1 := by use (k - φ m - 1) ; omega
-  induction j
-  case zero =>
-    have h1 : Nat.count (· ∈ range φ) (φ m + 1) = Nat.count (· ∈ range φ) (φ m) + 1 := by
-      apply Nat.count_succ_eq_succ_count ; use m
-    have h2 : Nat.count (· ∈ range φ) (φ m + 1 + 1) = Nat.count (· ∈ range φ) (φ m + 1) := by
-      apply Nat.count_succ_eq_count
-      apply strict_mono_range_gap hm (show φ m < φ m + 1 by omega) ; omega
-    have h3 := nth_of_strict_mono hm m
-    rw [segment, zero_add, h2, h1, Nat.add_one_sub_one, h3]
-    apply Nat.count_nth_of_infinite (strict_mono_infinite hm)
-  case succ j h_ind =>
-  specialize h_ind (by omega) (by omega) (by omega) (by omega)
-  have h1 : Nat.count (· ∈ range φ) (j + 1 + φ m + 1) = Nat.count (· ∈ range φ) (j + 1 + φ m) := by
-    apply Nat.count_succ_eq_count
-    apply strict_mono_range_gap hm (show φ m < j + 1 + φ m by omega) ; omega
-  have h2 : Nat.count (· ∈ range φ) (j + 1 + φ m + 1 + 1) =
-    Nat.count (· ∈ range φ) (j + 1 + φ m + 1) := by
-    apply Nat.count_succ_eq_count
-    apply strict_mono_range_gap hm (show φ m < j + 1 + φ m + 1 by omega) ; omega
-  rw [segment, (show j + «φ» m + 1 = j + 1 + φ m by omega), h1] at h_ind
-  rw [segment, h2, h1, h_ind]
+  · obtain ⟨j, h_j, rfl⟩ : ∃ j < φ (m + 1) - φ m - 1, k = j + φ m + 1 := ⟨k - φ m - 1, by omega⟩
+    induction j
+    case zero =>
+      have : Nat.count (· ∈ range φ) (φ m + 1 + 1) = Nat.count (· ∈ range φ) (φ m + 1) := by
+        have := strict_mono_range_gap hm (show φ m < φ m + 1 by grind)
+        grind
+      have := nth_of_strict_mono hm m
+      grind [Nat.count_nth_of_infinite, strict_mono_infinite]
+    case succ j _ =>
+      have := strict_mono_range_gap hm (show φ m < j + 1 + φ m     by grind)
+      have := strict_mono_range_gap hm (show φ m < j + 1 + φ m + 1 by grind)
+      grind
 
 /-- For a strictly monotonic function `φ : ℕ → ℕ` with `φ 0 = 0`,
 `φ` and `segment φ` form a Galois connection. -/
@@ -207,17 +186,15 @@ private lemma base_zero_strict_mono (hm : StrictMono φ) :
   have : φ 0 ≤ φ n := by simp [StrictMono.le_iff_le hm]
   omega
 
-open scoped Classical in
 /-- For a strictly monotonic function `φ : ℕ → ℕ`,
 `segment' φ` and `segment φ` are actually equal. -/
 theorem Nat.segment'_eq_segment (hm : StrictMono φ) :
     segment' φ = segment φ := by
+  classical
   ext k ; unfold segment'
   rcases (show k < φ 0 ∨ k ≥ φ 0 by omega) with h_k | h_k
-  · have h0 : segment (φ · - φ 0) (k - φ 0) = 0 := by
-      rw [show k - φ 0 = 0 by omega]
-      exact segment_zero (base_zero_strict_mono hm) (base_zero_shift φ)
-    rw [h0, segment_pre_zero hm h_k]
+  · have : k - φ 0 = 0 := by grind
+    grind
   unfold segment ; congr 1
   simp only [Nat.count_eq_card_filter_range]
   suffices h : ∃ f, BijOn f
@@ -225,27 +202,22 @@ theorem Nat.segment'_eq_segment (hm : StrictMono φ) :
       ({x ∈ Finset.range (k + 1) | x ∈ range φ}).toSet by
     obtain ⟨f, h_bij⟩ := h
     exact BijOn.finsetCard_eq f h_bij
-  use (fun n ↦ n + φ 0) ; unfold BijOn ; constructorm* _ ∧ _
+  refine ⟨fun n ↦ n + φ 0, ?_, ?_, ?_⟩
   · intro n ; simp only [mem_range, Finset.coe_filter, Finset.mem_range, mem_setOf_eq]
     rintro ⟨h_n, i, rfl⟩
     have := StrictMono.monotone hm <| zero_le i
-    constructor
-    · omega
-    · use i ; omega
-  · apply injOn_of_injective ; intro i j ; grind
+    refine ⟨?_, i, ?_⟩ <;> omega
+  · grind [injOn_of_injective, Injective]
   · intro n ; simp only [mem_range, Finset.coe_filter, Finset.mem_range, mem_setOf_eq, mem_image]
     rintro ⟨h_n, i, rfl⟩
     have := StrictMono.monotone hm <| zero_le i
-    use (φ i - φ 0) ; constructorm* _ ∧ _
-    · omega
-    · use i
-    · omega
+    refine ⟨φ i - φ 0, ⟨by omega, i, rfl⟩, by omega⟩
 
 /-- For a strictly monotonic function `φ : ℕ → ℕ`, `segment φ k = 0` for all `k ≤ φ 0`. -/
 theorem Nat.segment_zero' (hm : StrictMono φ) {k : ℕ} (h : k ≤ φ 0) :
     segment φ k = 0 := by
   rw [← segment'_eq_segment hm, segment', (show k - φ 0 = 0 by omega)]
-  exact segment_zero (base_zero_strict_mono hm) (base_zero_shift φ)
+  grind
 
 /-- For a strictly monotonic function `φ : ℕ → ℕ`, `k < φ (segment φ k + 1)` for all `k ≥ φ 0`. -/
 theorem Nat.segment_upper_bound' (hm : StrictMono φ) {k : ℕ} (h : φ 0 ≤ k) :
@@ -260,5 +232,3 @@ theorem Nat.segment_lower_bound' (hm : StrictMono φ) {k : ℕ} (h : φ 0 ≤ k)
   rw [← segment'_eq_segment hm, segment']
   have := segment_lower_bound (base_zero_strict_mono hm) (base_zero_shift φ) (k - φ 0)
   omega
-
-end Cslib
