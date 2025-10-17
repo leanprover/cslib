7cc01828405365dfcc87bde17bbf1b0135582968
Ching-Tsun Chou <chingtsun.chou@gmail.com>
2025-10-16 17:47:36 -0700

    Incorporate Chris Henson's comments on Cslib/Foundations/Data/OmegaSequence/Flatten.lean

--- Cslib/Foundations/Data/OmegaSequence/Flatten.lean
+++ Cslib/Foundations/Data/OmegaSequence/Flatten.lean
@@ -17,7 +17,7 @@ formed by the concatenation of all of them.
 
 namespace Cslib
 
-open Nat Function
+open Nat Function Set
 
 namespace ωSequence
 
@@ -37,24 +37,19 @@ theorem cumLen_zero {ls : ωSequence (List α)} :
     ls.cumLen 0 = 0 :=
   rfl
 
+@[scoped grind =]
 theorem cumLen_succ (ls : ωSequence (List α)) (k : ℕ) :
     ls.cumLen (k + 1) = ls.cumLen k + (ls k).length :=
   rfl
 
-theorem cumLen_1plus_drop (ls : ωSequence (List α)) (k : ℕ) :
+theorem cumLen_one_add_drop (ls : ωSequence (List α)) (k : ℕ) :
     ls.cumLen (1 + k) = (ls 0).length + (ls.drop 1).cumLen k := by
-  induction k
-  case zero => simp [cumLen_zero, cumLen_succ]
-  case succ k h_ind =>
-  rw [← add_assoc, cumLen_succ, h_ind, cumLen_succ, add_assoc]
-  congr 2 ; rw [get_drop]
+  induction k <;> grind
 
 /-- If all lists in `ls` are nonempty, then `ls.cumLen` is strictly monotonic. -/
 theorem cumLen_strict_mono {ls : ωSequence (List α)} (h_ls : ∀ k, (ls k).length > 0) :
     StrictMono ls.cumLen := by
-  apply strictMono_nat_of_lt_succ
-  intro k ; have := h_ls k
-  rw [cumLen_succ] ; omega
+  grind [strictMono_nat_of_lt_succ]
 
 @[simp, scoped grind =]
 theorem cumLen_segment_zero {ls : ωSequence (List α)} (h_ls : ∀ k, (ls k).length > 0)
@@ -63,10 +58,10 @@ theorem cumLen_segment_zero {ls : ωSequence (List α)} (h_ls : ∀ k, (ls k).le
   have h1 : n < ls.cumLen 1 := by simpa [cumLen_succ, cumLen_zero]
   exact segment_range_val (cumLen_strict_mono h_ls) h0 h1
 
-open scoped Classical in
-theorem cumLen_segment_1plus {ls : ωSequence (List α)} (h_ls : ∀ k, (ls k).length > 0)
+theorem cumLen_segment_one_add {ls : ωSequence (List α)} (h_ls : ∀ k, (ls k).length > 0)
     (n : ℕ) (h_n : (ls 0).length ≤ n) :
     segment ls.cumLen n = 1 + segment (ls.drop 1).cumLen (n - (ls 0).length) := by
+  classical
   have h0 : (ls.drop 1).cumLen 0 = 0 := by simp [cumLen_zero]
   rw [add_comm, segment_plus_one h0] ; unfold Nat.segment
   simp only [Nat.count_eq_card_filter_range]
@@ -77,38 +72,24 @@ theorem cumLen_segment_1plus {ls : ωSequence (List α)} (h_ls : ∀ k, (ls k).l
     · rintro ⟨h_k, i, rfl⟩
       simp only [h_k, exists_apply_eq_apply, true_and, or_iff_not_imp_left]
       intro h_i
-      suffices h : i = 1 + (i - 1) by rw [h, cumLen_1plus_drop] ; omega
-      suffices h : 0 < i by omega
-      rw [← StrictMono.lt_iff_lt (cumLen_strict_mono h_ls), cumLen_zero] ; omega
+      suffices h : i = 1 + (i - 1) by grind [cumLen_one_add_drop]
+      grind
     · rintro (rfl | _)
-      · simp only [lt_add_iff_pos_left, add_pos_iff, zero_lt_one, or_true, true_and]
-        use 0 ; rw [cumLen_zero]
+      · refine ⟨?_, 0, ?_⟩ <;> grind
       · grind
   have h2 : 0 ∉ {x ∈ Finset.range (n + 1) | x ∈ Set.range ls.cumLen ∧ (ls 0).length ≤ x} := by
-    suffices h : ¬ (ls 0).length ≤ 0 by simp [h]
     grind
-  rw [h1, Finset.card_insert_of_notMem h2, Nat.add_one_sub_one] ; symm
+  rw [h1, Finset.card_insert_of_notMem h2, Nat.add_one_sub_one]
+  symm
   apply Set.BijOn.finsetCard_eq (fun n ↦ n + (ls 0).length)
-  unfold Set.BijOn ; constructorm* _ ∧ _
-  · intro k ; simp only [Set.mem_range, Finset.coe_filter, Finset.mem_range, Set.mem_setOf_eq,
-      le_add_iff_nonneg_left, _root_.zero_le, and_true]
-    rintro ⟨h_k, i, rfl⟩ ; constructorm* _ ∧ _
-    · omega
-    · use (1 + i) ; rw [cumLen_1plus_drop, add_comm]
-  · apply Set.injOn_of_injective ; intro i j ; grind
-  · intro k
-    simp only [Set.mem_range, Finset.coe_filter, Finset.mem_range, Set.mem_setOf_eq, Set.mem_image]
-    rintro ⟨h_k, ⟨i, rfl⟩, h_l0⟩
-    use (ls.cumLen i - (ls 0).length)
-    simp (disch := omega) only [Nat.sub_add_cancel, and_true] ; constructor
-    · omega
-    · use (i - 1)
-      have := cumLen_1plus_drop ls (i - 1)
-      have : 1 + (i - 1) = i := by
-        suffices 0 < i by omega
-        rw [← StrictMono.lt_iff_lt (cumLen_strict_mono h_ls), cumLen_zero]
-        grind
-      grind
+  refine ⟨?_, by grind [injOn_of_injective, Injective], ?_⟩ <;>
+  ( intro k ; simp only [Set.mem_range, Finset.coe_filter, Finset.mem_range, Set.mem_setOf_eq,
+      le_add_iff_nonneg_left, _root_.zero_le, and_true] )
+  · rintro ⟨h_k, i, rfl⟩
+    refine ⟨?_, 1 + i, ?_⟩ <;> grind [cumLen_one_add_drop]
+  · rintro ⟨h_k, ⟨i, rfl⟩, h_l0⟩
+    have := cumLen_one_add_drop ls (i - 1)
+    refine ⟨ls.cumLen i - (ls 0).length, ⟨?_, i - 1, ?_⟩, ?_⟩ <;> grind
 
 /-- Given an ω-sequence `ls` of lists, `ls.flatten` is the infinite sequence
 formed by the concatenation of all of them.  For the definition to make proper
@@ -127,23 +108,14 @@ theorem flatten_cons [Inhabited α] {ls : ωSequence (List α)} (h_ls : ∀ k, (
   ext n ; rw [flatten_def, head, tail_eq_drop]
   rcases (show n < (ls 0).length ∨ n ≥ (ls 0).length by omega) with h_n | h_n
   · simp (disch := omega) [get_append_left, cumLen_segment_zero, cumLen_zero]
-  · simp (disch := omega) [get_append_right', flatten_def, cumLen_segment_1plus, cumLen_1plus_drop]
-    congr 2 ; omega
+  · simp (disch := omega) [get_append_right', flatten_def, cumLen_segment_one_add, cumLen_one_add_drop]
+    grind
 
 /-- `ls.flatten` equals the concatenation of `(ls.take n).flatten` and `(ls.drop n).flatten`. -/
 @[simp, scoped grind =]
 theorem flatten_append [Inhabited α] {ls : ωSequence (List α)} (h_ls : ∀ k, (ls k).length > 0)
     (n : ℕ) : (ls.take n).flatten ++ω (ls.drop n).flatten = ls.flatten := by
-  revert ls
-  induction n
-  case zero => simp
-  case succ n h_ind =>
-  intro ls h_ls
-  specialize h_ind (ls := ls.drop 1) (by grind)
-  rw [drop_drop, add_comm] at h_ind
-  rw [take_succ, List.flatten_cons, append_append_ωSequence, tail_eq_drop,
-    h_ind, ← tail_eq_drop]
-  apply flatten_cons h_ls
+  induction n generalizing ls <;> grind [tail_eq_drop, take_succ]
 
 end ωSequence
 
