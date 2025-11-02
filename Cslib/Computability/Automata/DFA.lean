39181f4a0083df224f1819c55d2f1df46be6d1f2
Fabrizio Montesi <famontesi@gmail.com>
2025-11-02 17:49:40 +0100

    Unbundle typeclasses from DFA

--- Cslib/Computability/Automata/DFA.lean
+++ Cslib/Computability/Automata/DFA.lean
@@ -14,7 +14,7 @@ import Cslib.Computability.Languages.Language
 A Deterministic Finite Automaton (DFA) is a finite-state machine that accepts or rejects
 a finite string.
 
-This is the bundled version of `DA` with an exact acceptor and finiteness assumptions.
+This is the bundled version of `DA` with a set of accept states and finiteness assumptions.
 It is provided as a utility for applications and as a reference to the established terminology in
 the literature. To prove results about this kind of machines, please use the unbundled version `DA`
 and be precise with the required assumptions.
@@ -27,20 +27,16 @@ and be precise with the required assumptions.
 
 namespace Cslib.Automata
 
-/-- A Deterministic Finite Automaton (DFA) consists of a `DA` with finite sets of states and symbols
+/-- A Deterministic Finite Automaton (DFA) consists is a `DA` with finite sets of states and symbols
 equipped with a finite set of accepting states `accept`. -/
-structure DFA (State : Type _) (Symbol : Type _) where
-  /-- The underlying `DA`. -/
-  da : DA State Symbol
+structure DFA (State : Type _) (Symbol : Type _) [Finite State] [Finite Symbol]
+    extends DA State Symbol where
   /-- The set of accepting states of the automaton. -/
   accept : Set State
-  /-- The automaton is finite-state. -/
-  finite_state : Finite State
-  /-- The type of symbols (also called 'alphabet') is finite. -/
-  finite_symbol : Finite Symbol
 
 /-- Returns the `Acceptor` based on a `DFA`. -/
-def DFA.acceptor {State : Type _} {Symbol : Type _} (dfa : DFA State Symbol) : Acceptor Symbol :=
-  dfa.da.exactAcceptor dfa.accept
+def DFA.acceptor {State : Type _} {Symbol : Type _} [Finite State] [Finite Symbol]
+    (dfa : DFA State Symbol) : Acceptor Symbol :=
+  dfa.exactAcceptor dfa.accept
 
 end Cslib.Automata
