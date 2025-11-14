1a35531121cf995d5b3b7b861423e0768db40144
Fabrizio Montesi <famontesi@gmail.com>
2025-11-14 21:00:50 +0100

    feat (LTS, Bisimulation): move to classes and scoped grind

--- Cslib/Languages/CCS/BehaviouralTheory.lean
+++ Cslib/Languages/CCS/BehaviouralTheory.lean
@@ -184,6 +184,7 @@ private inductive ChoiceComm : Process Name Constant → Process Name Constant �
   | choiceComm : ChoiceComm (choice p q) (choice q p)
   | bisim : (p ~[lts (defs := defs)] q) → ChoiceComm p q
 
+open Bisimilarity LTS in
 /-- P + Q ~ Q + P -/
 theorem bisimilarity_choice_comm : (choice p q) ~[lts (defs := defs)] (choice q p) := by
   exists @ChoiceComm Name Constant defs
@@ -438,7 +439,7 @@ theorem bisimilarity_congr
       _ ~[lts (defs := defs)] (c.fill p |>.choice r)  := by grind [bisimilarity_choice_comm]
       _ ~[lts (defs := defs)] (c.fill q |>.choice r)  := by grind [bisimilarity_congr_choice]
       _ ~[lts (defs := defs)] (c.choiceR r |>.fill q) := by grind [bisimilarity_choice_comm]
-  | _ => grind [bisimilarity_congr_pre, bisimilarity_congr_par, 
+  | _ => grind [bisimilarity_congr_pre, bisimilarity_congr_par,
                 bisimilarity_congr_choice, bisimilarity_congr_res]
 
 end CCS
