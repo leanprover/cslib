133deeaa1a9f56d0579dfeabcb9d07c8d2bfcf17
Chris Henson <chrishenson.net@gmail.com>
2025-11-14 07:45:14 -0500

    combine adjacent simp and grind

--- Cslib/Foundations/Semantics/LTS/LTSToFLTS.lean
+++ Cslib/Foundations/Semantics/LTS/LTSToFLTS.lean
@@ -28,8 +28,7 @@ original `LTS`. -/
 @[scoped grind =]
 theorem toFLTS_mem_mtr {lts : LTS State Label} {S : Set State} {s' : State} {μs : List Label} :
   s' ∈ lts.toFLTS.mtr S μs ↔ ∃ s ∈ S, lts.MTr s μs s' := by
-  simp only [LTS.toFLTS, FLTS.mtr]
-  grind
+  grind [LTS.toFLTS, FLTS.mtr]
 
 /-- Characterisation of multistep transitions in `LTS.toFLTS` as image transitions in `LTS`. -/
 @[scoped grind =]
