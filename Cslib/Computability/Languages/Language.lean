9022ec0fdfc678ba5584fc2b9e206ac48ab3d6ec
Chris Henson <chrishenson.net@gmail.com>
2025-11-19 04:29:31 -0500

    remove now redundant instances

--- Cslib/Computability/Languages/Language.lean
+++ Cslib/Computability/Languages/Language.lean
@@ -39,19 +39,6 @@ theorem mem_biSup {I : Type*} (s : Set I) (l : I → Language α) (x : List α) 
   · apply mem_iUnion₂.mpr
     grind
 
--- This section will be removed once the following PR gets into mathlib:
--- https://github.com/leanprover-community/mathlib4/pull/30913
-section from_mathlib4_30913
-
-/-- The subtraction of two languages is their difference. -/
-instance : Sub (Language α) where
-  sub := SDiff.sdiff
-
-instance : OrderedSub (Language α) where
-  tsub_le_iff_right _ _ _ := sdiff_le_iff'
-
-end from_mathlib4_30913
-
 theorem le_one_iff_eq : l ≤ 1 ↔ l = 0 ∨ l = 1 :=
   subset_singleton_iff_eq
 
