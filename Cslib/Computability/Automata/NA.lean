0efad79d37d4e80515d51eabdff49ec2eaffb5da
Ching-Tsun Chou <chingtsun.chou@gmail.com>
2025-11-20 10:22:29 -0800

    minor modification

--- Cslib/Computability/Automata/NA.lean
+++ Cslib/Computability/Automata/NA.lean
@@ -1,7 +1,7 @@
 /-
 Copyright (c) 2025 Fabrizio Montesi. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
-Authors: Fabrizio Montesi, Ching-Tsun Chou
+Authors: Fabrizio Montesi, Ching-Tsun Chou, Chris Henson.
 -/
 
 import Cslib.Computability.Automata.Acceptor
