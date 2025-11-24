435f3a977e65cad9d3b8bbdbddee14e4f24282be
Chris Henson <chrishenson.net@gmail.com>
2025-11-24 13:32:58 -0500

    perf: remove all uses of `Type _`

--- Cslib/Foundations/Data/Relation.lean
+++ Cslib/Foundations/Data/Relation.lean
@@ -52,7 +52,7 @@ theorem Relation.ReflTransGen.diamond_confluence (h : Diamond R) : Confluence R 
     exact ⟨D', ⟨B_D', ReflTransGen.trans CD D_D'⟩⟩
 
 -- not sure why this doesn't compile as an "instance" but oh well
-def trans_of_subrelation {α : Type _} (s s' r : α → α → Prop) (hr : Transitive r)
+def trans_of_subrelation {α : Type*} (s s' r : α → α → Prop) (hr : Transitive r)
     (h : ∀ a b : α, s a b → r a b) (h' : ∀ a b : α, s' a b → r a b) : Trans s s' r where
   trans := by
     intro a b c hab hbc
@@ -60,14 +60,14 @@ def trans_of_subrelation {α : Type _} (s s' r : α → α → Prop) (hr : Trans
     replace hbc := h' _ _ hbc
     exact hr hab hbc
 
-def trans_of_subrelation_left {α : Type _} (s r : α → α → Prop) (hr : Transitive r)
+def trans_of_subrelation_left {α : Type*} (s r : α → α → Prop) (hr : Transitive r)
     (h : ∀ a b : α, s a b → r a b) : Trans s r r where
   trans := by
     intro a b c hab hbc
     replace hab := h _ _ hab
     exact hr hab hbc
 
-def trans_of_subrelation_right {α : Type _} (s r : α → α → Prop) (hr : Transitive r)
+def trans_of_subrelation_right {α : Type*} (s r : α → α → Prop) (hr : Transitive r)
     (h : ∀ a b : α, s a b → r a b) : Trans r s r where
   trans := by
     intro a b c hab hbc
@@ -76,7 +76,7 @@ def trans_of_subrelation_right {α : Type _} (s r : α → α → Prop) (hr : Tr
 
 /-- This is a straightforward but useful specialisation of a more general result in
 `Mathlib.Logic.Relation`. -/
-theorem church_rosser_of_diamond {α : Type _} {r : α → α → Prop}
+theorem church_rosser_of_diamond {α : Type*} {r : α → α → Prop}
     (h : ∀ a b c, r a b → r a c → Relation.Join r b c) :
     Equivalence (Relation.Join (Relation.ReflTransGen r)) := by
   apply Relation.equivalence_join_reflTransGen
