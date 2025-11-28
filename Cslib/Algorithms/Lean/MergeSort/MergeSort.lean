3427609ef87c30a8d360c3dc85981ea420ecd1f3
Sorrachai Yingchareonthawornchai <sorrachai.cp@gmail.com>
2025-11-28 16:02:21 +0100

    TimeMonad and MergeSort as separate files

--- Cslib/Algorithms/Lean/MergeSort/MergeSort.lean
+++ Cslib/Algorithms/Lean/MergeSort/MergeSort.lean
@@ -1,60 +1,47 @@
+/-
+Copyright (c) 2025 Sorrachai Yingchareonthawornhcai. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Sorrachai Yingchareonthawornhcai
+-/
+
 import Mathlib.Tactic
+import Cslib.Algorithms.Lean.TimeMonad
+
+
+/-!
+# MergeSort on a list
+
+In this file we introduce `merge` and `mergeSort` algorithms that returns a time
+monad over the list `TimeM (List α)`.
+
+--
+## Main results
+
+- `MSCorrect`: `mergeSort` returns a sorted list which is a permutation of the input list.
+- `MSTime`:  `mergeSort` time is `n *(Nat.log 2 n)` when `n`, the list's size, is a power of two.
+
+-/
 
 set_option tactic.hygienic false
 set_option autoImplicit false
 
-structure TimeM (α : Type) where
-  ret : α
-  time : ℕ
-
 namespace TimeM
+section MergeSort
 
-def pure {α} (a : α) : TimeM α :=
-  ⟨a, 0⟩
+variable {α : Type} [LinearOrder α]
 
-def bind {α β} (m : TimeM α) (f : α → TimeM β) : TimeM β :=
-  let r := f m.ret
-  ⟨r.ret, m.time + r.time⟩
-
-instance : Monad TimeM where
-  pure := pure
-  bind := bind
-
--- Increment time
-@[simp] def tick {α : Type} (a : α) (c : ℕ := 1) : TimeM α :=
-  ⟨a, c⟩
-
-notation "✓" a:arg ", " c:arg => tick a c
-notation "✓" a:arg => tick a  -- Default case with only one argument
-
-def tickUnit : TimeM Unit :=
-  ✓ () -- This uses the default time increment of 1
-
-@[grind, simp] theorem time_of_pure {α} (a : α) : (pure a).time = 0 := rfl
-@[grind, simp] theorem time_of_bind {α β} (m : TimeM α) (f : α → TimeM β) :
- (TimeM.bind m f).time = m.time + (f m.ret).time := rfl
-@[grind, simp] theorem time_of_tick {α} (a : α) (c : ℕ) : (tick a c).time = c := rfl
-@[grind, simp] theorem ret_bind {α β} (m : TimeM α) (f : α → TimeM β) :
-  (TimeM.bind m f).ret = (f m.ret).ret := rfl
-
--- allow us to simplify the chain of compositions
-attribute [simp] Bind.bind Pure.pure TimeM.pure
-
--- Analysis of Merge Sort
-def merge (xs ys : List ℕ) : TimeM (List ℕ) :=
-  let rec go : List ℕ → List ℕ → TimeM (List ℕ)
+def merge :  List α → List α  → TimeM (List α)
   | [], ys => ✓ ys, ys.length
   | xs, [] => ✓ xs, xs.length
   | x::xs', y::ys' => do
-    if x≤ y then
-      let rest ← go xs' (y::ys')
+    if x ≤ y then
+      let rest ← merge xs' (y::ys')
       ✓ (x :: rest)
     else
-      let rest ← go (x::xs') ys'
+      let rest ← merge (x::xs') ys'
       ✓ (y :: rest)
-  go xs ys
 
-def mergeSort (xs : List ℕ) : TimeM (List ℕ) :=  do
+def mergeSort (xs : List α) : TimeM (List α) :=  do
   if xs.length < 2 then return xs
   else
     let n := xs.length
@@ -65,104 +52,95 @@ def mergeSort (xs : List ℕ) : TimeM (List ℕ) :=  do
     let sortedRight ← mergeSort right
     merge sortedLeft sortedRight
 
-#check mergeSort
-#eval merge [1,2,3,10] [4,5]
-#eval mergeSort [4,2,3,1]
-
 open List
 
-@[simp, grind] def IsSorted (l : List ℕ) : Prop := Sorted (· ≤ ·) l
-@[simp, grind] def MinOfList (x : ℕ) (l : List ℕ) : Prop := ∀ b ∈ l, x ≤ b
+@[simp, grind] def IsSorted (l : List α) : Prop := Sorted (· ≤ ·) l
+@[simp, grind] def MinOfList (x : α) (l : List α) : Prop := ∀ b ∈ l, x ≤ b
 
-theorem mem_either_merge (xs ys : List ℕ) (z : ℕ)
+theorem mem_either_merge (xs ys : List α) (z : α)
   (hz : z ∈ (merge xs ys).ret) : z ∈ xs ∨ z ∈ ys := by
-  rw [merge] at hz
-  fun_induction merge.go
+  fun_induction merge
   all_goals (try (simp only [not_mem_nil, false_or];assumption))
   all_goals
   simp only [Bind.bind, tick, ret_bind, mem_cons] at hz
   cases hz
   all_goals (aesop)
 
-
-theorem min_all_merge (x : ℕ) (xs ys : List ℕ)
+theorem min_all_merge (x : α) (xs ys : List α)
 (hxs : MinOfList x xs) (hys : MinOfList x ys) : MinOfList x (merge xs ys).ret := by
   simp_all only [MinOfList]
   intro a ha
   apply mem_either_merge at ha
   grind
 
-theorem sorted_merge {l1 l2 : List ℕ} (hxs : IsSorted l1)
+theorem sorted_merge {l1 l2 : List α} (hxs : IsSorted l1)
   (hys : IsSorted l2) : IsSorted ((merge l1 l2).ret) := by
-  fun_induction merge.go l1 l2 <;> all_goals (first| simpa [merge,merge.go,tick]|
-    simp only [IsSorted, merge, merge.go, h, ↓reduceIte, Bind.bind, tick, ret_bind, sorted_cons]
-    simp [merge] at ih1
-    simp_all only [IsSorted, sorted_cons, implies_true, forall_const, and_true]
-    apply min_all_merge <;> all_goals grind
-  )
+  fun_induction merge l1 l2 <;> try simpa
+  all_goals (simp_all only [IsSorted, sorted_cons, and_imp, Bind.bind, tick, Nat.cast_one, ret_bind,
+      implies_true, and_true, forall_const]; apply min_all_merge <;> grind)
 
-theorem MSMCorrect (xs : List ℕ) : IsSorted (mergeSort xs).ret := by
+theorem MSCorrect_sorted (xs : List α) : IsSorted (mergeSort xs).ret := by
   fun_induction mergeSort xs
-  · obtain h | h : x = [] ∨ ∃a, x = [a] := by
-      by_cases x.length = 0
-      · aesop
-      · right
-        have: x.length = 1 := by omega
-        exact List.length_eq_one_iff.mp this
-    · subst h
-      simp only [IsSorted]
-      exact sorted_nil
-    · obtain ⟨a,ha⟩ := h
-      rw [ha]
-      exact sorted_singleton a
+  · simp only [IsSorted, Pure.pure, pure]
+    rcases x with _ | ⟨a, _ | ⟨b, rest⟩⟩ <;> simp [sorted_nil,sorted_singleton]; grind
   · simp only [IsSorted, Bind.bind, ret_bind]
     exact sorted_merge ih2 ih1
 
-@[simp] theorem merge_time (xs ys : List ℕ) :
-  (merge xs ys).time = xs.length + ys.length := by
-  simp only [merge]
-  fun_induction merge.go <;>
-  all_goals(simp_all only [length_cons, Bind.bind, tick, time_of_bind]; all_goals (grind))
+lemma merge_perm (l₁ l₂ : List α) : (merge l₁ l₂).ret ~ l₁ ++ l₂ := by
+  fun_induction merge <;> simp only [Bind.bind, tick, ret_bind, cons_append, perm_cons] <;> grind
 
-@[simp] theorem merge_ret_length_eq_sum (xs ys : List ℕ) :
+theorem MSCorrect_perm (xs : List α) : (mergeSort xs).ret ~ xs := by
+  fun_induction mergeSort xs
+  · simp only [Pure.pure, pure, Perm.refl]
+  · simp only [Bind.bind, ret_bind]
+    calc
+      (merge (mergeSort left).ret (mergeSort right).ret).ret ~
+      (mergeSort left).ret ++ (mergeSort right).ret  := by apply merge_perm
+      _ ~ left++right := Perm.append ih2 ih1
+      _ ~ x := by simp only [take_append_drop, Perm.refl, left, right]
+
+theorem MSCorrect (xs : List α) : IsSorted (mergeSort xs).ret ∧ (mergeSort xs).ret ~ xs  := by
+  constructor
+  · apply MSCorrect_sorted
+  · apply MSCorrect_perm
+
+@[grind =] def MS_REC : ℕ → ℤ
+| 0 => 0
+| 1 => 0
+| n@(_+2) => MS_REC (n/2) + MS_REC ((n-1)/2 + 1) + n
+
+@[simp] theorem merge_ret_length_eq_sum (xs ys : List α) :
   (merge xs ys).ret.length = xs.length + ys.length := by
-  rw [merge]
-  fun_induction merge.go
-  all_goals
+  fun_induction merge <;> all_goals
   simp_all only [length_cons, Bind.bind, tick, ret_bind]
   grind
 
-@[simp] theorem mergeSort_same_length (xs : List ℕ) :
+@[simp] theorem mergeSort_same_length (xs : List α) :
   (mergeSort xs).ret.length = xs.length:= by
   fun_induction mergeSort
   · simp only [Pure.pure, pure]
   · simp only [Bind.bind, ret_bind, merge_ret_length_eq_sum]
     grind
 
+@[simp] theorem merge_time (xs ys : List α) :
+  (merge xs ys).time = xs.length + ys.length := by
+  fun_induction merge <;> simp_all only [tick, length_nil, CharP.cast_eq_zero, zero_add,add_zero]
+  all_goals (simp_all;grind)
 
-@[grind =] def MS_REC : ℕ → ℕ
-| 0 => 0
-| 1 => 0
-| n@(_+2) => MS_REC (n/2) + MS_REC ((n-1)/2 + 1) + n
-
-#eval mergeSort [4,1,3,4,5,6]
-#eval MS_REC 6
-
-theorem mergeSort_time_eq_MS_REC (xs : List ℕ) :
+theorem mergeSort_time_eq_MS_REC (xs : List α) :
   (mergeSort xs).time = MS_REC xs.length := by
   fun_induction mergeSort
   · simp only [Pure.pure, pure]
     unfold MS_REC
+    norm_cast
     grind
-  · simp_all only [not_lt, Bind.bind, time_of_bind, merge_time, mergeSort_same_length]
-    conv =>
-      right
-      unfold MS_REC
-    simp_all only [Nat.ofNat_pos, Nat.add_div_right, Nat.add_one_sub_one]
-    split <;> all_goals (grind)
+  · simp only [Bind.bind, time_of_bind, merge_time, mergeSort_same_length]
+    unfold MS_REC
+    split <;> all_goals (simp_all)
+    norm_cast
+    grind
 
-
-@[grind =] def MS_REC_SIMP : ℕ → ℕ
+@[grind =] def MS_REC_SIMP : ℕ → ℤ
 | 0 => 0
 | 1 => 0
 | n@(_+2) => 2*MS_REC_SIMP (n/2) + n
@@ -180,11 +158,14 @@ theorem MS_REC_SIMP_EQ_CLOSED (n : ℕ) : MS_REC_SIMP (2^n) = 2^n * n := by
     split <;> grind
 
 -- The time is written assuming the length of the list is a power of two (for simplicity).
-theorem MStimeBound (xs : List ℕ) (h : ∃ k, xs.length = 2 ^ k) :
+theorem MSTime (xs : List α) (h : ∃ k, xs.length = 2 ^ k) :
   (mergeSort xs).time = xs.length*(Nat.log 2 xs.length)  := by
   rw [mergeSort_time_eq_MS_REC]
   obtain ⟨k,hk⟩ := h
   rw [hk,MS_REC_SIMP_EQ,MS_REC_SIMP_EQ_CLOSED]
   simp only [Nat.lt_add_one, Nat.log_pow]
+  norm_cast
+
+end MergeSort
 
 end TimeM
---
+++ Cslib/Algorithms/Lean/TimeMonad.lean
@@ -0,0 +1,64 @@
+/-
+Copyright (c) 2025 Sorrachai Yingchareonthawornhcai. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Sorrachai Yingchareonthawornhcai
+-/
+
+import Mathlib.Tactic
+
+/-!
+# TimeMonad
+
+`TimeM` is a monad that tracks execution time alongside computations.
+Time is represented as rational numbers (`ℚ`) to facilitate
+manipulation of recurrence relations.
+
+## Notation
+
+ - `✓` : a tick of time, see `tick`.
+
+## References
+
+See [Danielsson'08] for the discussion.
+-/
+
+set_option tactic.hygienic false
+set_option autoImplicit false
+
+structure TimeM (α : Type) where
+  ret : α
+  time : ℚ
+
+namespace TimeM
+
+def pure {α} (a : α) : TimeM α :=
+  ⟨a, 0⟩
+
+def bind {α β} (m : TimeM α) (f : α → TimeM β) : TimeM β :=
+  let r := f m.ret
+  ⟨r.ret, m.time + r.time⟩
+
+instance : Monad TimeM where
+  pure := pure
+  bind := bind
+
+@[simp] def tick {α : Type} (a : α) (c : ℕ := 1) : TimeM α :=
+  ⟨a, c⟩
+
+scoped notation "✓" a:arg ", " c:arg => tick a c
+scoped notation "✓" a:arg => tick a  -- Default case with only one argument
+
+def tickUnit : TimeM Unit :=
+  ✓ () -- This uses the default time increment of 1
+
+@[simp] theorem time_of_pure {α} (a : α) : (pure a).time = 0 := rfl
+@[simp] theorem time_of_bind {α β} (m : TimeM α) (f : α → TimeM β) :
+ (TimeM.bind m f).time = m.time + (f m.ret).time := rfl
+@[simp] theorem time_of_tick {α} (a : α) (c : ℕ) : (tick a c).time = c := rfl
+@[simp] theorem ret_bind {α β} (m : TimeM α) (f : α → TimeM β) :
+  (TimeM.bind m f).ret = (f m.ret).ret := rfl
+
+-- this allow us to simplify the chain of compositions
+attribute [simp] Bind.bind Pure.pure TimeM.pure
+
+end TimeM
