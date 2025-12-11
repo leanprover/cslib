cbf94f64a49c5983dec67a090c8f5bcaf186c6eb
Bolton Bailey <bolton.bailey@gmail.com>
2025-12-11 11:41:37 -0800

    additional development

--- Cslib/Computability/Languages/ComplexityClass.lean
+++ Cslib/Computability/Languages/ComplexityClass.lean
@@ -24,30 +24,17 @@ namespace ComplexityTheory
 The type of decision problems on bitstrings.
 
 We define these as functions from lists of booleans to booleans,
-implictly assuming the usual encodings.
+implicitly assuming the usual encodings.
 
 TODO: An Decision Problem type over arbitrary types.
 -/
-abbrev BitstringDecisionProblem := List Bool → Bool
-
-/--
-The type of complexity classes over bitstrings.
-We define these as sets of `BitstringDecisionProblem`s.
--/
-abbrev ComplexityClass := Set BitstringDecisionProblem
+abbrev BitstringDecisionProblem : Type := List Bool → Bool
 
 /--
 A simple definition to abstract the notion of a poly-time Turing machine into a predicate.
 -/
 def IsComputableInPolyTime {α β : Type} (ea : FinEncoding α) (eb : FinEncoding β) (f : α → β) :=
   Nonempty (TM2ComputableInPolyTime ea eb f)
-
-/--
-The class P is the set of decision problems
-decidable in polynomial time by a deterministic Turing machine.
--/
-def P : ComplexityClass :=
-  { L | IsComputableInPolyTime finEncodingListBool finEncodingBoolBool L }
 
 /--
 The list of all bitstrings of exactly length `n`, in lexicographic order.
@@ -63,6 +50,11 @@ Ordered first by length, then lexicographically.
 -/
 def bitstringsUpToLength (n : ℕ) : List (List Bool) :=
   (List.range (n + 1)) >>= bitstringsOfLength
+
+@[simp]
+lemma bitstringsUpToLength_zero :
+    bitstringsUpToLength 0 = [[]] := by
+  rfl
 
 /--
 Given a polynomial `p` and a bitstring decision problem `L` that operates on pairs of bitstrings,
@@ -93,92 +85,77 @@ def BitstringDecisionProblem.existentiallyQuantifyOverPolynomial
   fun x ↦ List.any (bitstringsUpToLength (p.eval x.length)) fun w ↦
     L (encode_list_bool_prod_list_bool (x, w))
 
+@[simp]
+lemma BitstringDecisionProblem.universallyQuantifyOverPolynomial_complement
+    (p : Polynomial ℕ) (L : BitstringDecisionProblem) :
+    (L.universallyQuantifyOverPolynomial p)ᶜ =
+      (Lᶜ).existentiallyQuantifyOverPolynomial p := by
+  unfold universallyQuantifyOverPolynomial
+  unfold existentiallyQuantifyOverPolynomial
+  ext x
+  simp [List.not_any_eq_all_not] -- Add to push Bool.not
 
-def ComplexityClass.polyUniversallyQuantify (C : ComplexityClass) :
+@[simp]
+lemma BitstringDecisionProblem.existentiallyQuantifyOverPolynomial_complement
+    (p : Polynomial ℕ) (L : BitstringDecisionProblem) :
+    (L.existentiallyQuantifyOverPolynomial p)ᶜ =
+      (Lᶜ).universallyQuantifyOverPolynomial p := by
+  unfold universallyQuantifyOverPolynomial
+  unfold existentiallyQuantifyOverPolynomial
+  ext x
+  simp [List.not_all_eq_any_not] -- Add to push Bool.not
+
+
+
+/--
+The type of complexity classes over bitstrings.
+We define these as sets of `BitstringDecisionProblem`s.
+-/
+abbrev ComplexityClass : Type := Set BitstringDecisionProblem
+
+namespace ComplexityClass
+
+/--
+The class P is the set of decision problems
+decidable in polynomial time by a deterministic Turing machine.
+-/
+def P : ComplexityClass :=
+  { L | IsComputableInPolyTime finEncodingListBool finEncodingBoolBool L }
+
+/--
+The complement of a complexity class `C` is the set of decision problems
+whose complements are in `C`.
+
+Note that this is distinct from the complement of `C` as a set, which would be
+the set of decision problems not in `C`.
+-/
+def complement (C : ComplexityClass) : ComplexityClass :=
+  { L | (Lᶜ ∈ C) }
+
+@[simp]
+lemma complement_complement (C : ComplexityClass) :
+    C.complement.complement = C := by
+  ext L
+  simp [complement, Set.mem_setOf_eq, compl_compl]
+
+@[simp]
+lemma P_complement : P.complement = P := by
+  ext L
+  sorry
+
+def polyUniversallyQuantify (C : ComplexityClass) :
     ComplexityClass :=
   { L | ∃ p : Polynomial ℕ, ∃ L' ∈ C,
       L = BitstringDecisionProblem.universallyQuantifyOverPolynomial p L' }
 
-notation "∀ᴾ " C => ComplexityClass.polyUniversallyQuantify C
+notation "∀ᴾ " C => polyUniversallyQuantify C
 
-def ComplexityClass.polyExistentiallyQuantify (C : ComplexityClass) :
+def polyExistentiallyQuantify (C : ComplexityClass) :
     ComplexityClass :=
   { L | ∃ p : Polynomial ℕ, ∃ L' ∈ C,
       L = BitstringDecisionProblem.existentiallyQuantifyOverPolynomial p L' }
 
-notation "∃ᴾ " C => ComplexityClass.polyExistentiallyQuantify C
-
-
--- /--
--- The polynomial time hierarchy is defined by mutual induction as follows:
-
--- Σᴾ 0 = Πᴾ 0 = P
--- Σᴾ n+1 = ∃ᵖ Πᴾ n
--- Πᴾ n+1 = ∀ᵖ Σᴾ n
--- -/
-mutual
-  def SigmaPolyTimeHierarchy : ℕ → ComplexityClass
-    | 0     => P
-    | n + 1 => (PiPolyTimeHierarchy n).polyExistentiallyQuantify
-
-  def PiPolyTimeHierarchy : ℕ → ComplexityClass
-    | 0     => P
-    | n + 1 => (SigmaPolyTimeHierarchy n).polyUniversallyQuantify
-end
-
--- TODO bind more tightly
-notation "Σᴾ " n => SigmaPolyTimeHierarchy n
-notation "Πᴾ " n => PiPolyTimeHierarchy n
-
-def PolyTimeHierarchy : ComplexityClass :=
-  { L | ∃ n : ℕ, L ∈ Σᴾ n }
-
-lemma SigmaPolyTimeHierarchy_succ
-    (n : ℕ) : (Σᴾ (n + 1)) = (Πᴾ n).polyExistentiallyQuantify :=
-  rfl
-
-/--
-Pi n contained in Sigma n+1
--/
-lemma PiPolyTimeHierarchy_subset_SigmaPolyTimeHierarchy_succ
-    (n : ℕ) : (Πᴾ n) ⊆ Σᴾ (n + 1) := by
-  rw [SigmaPolyTimeHierarchy_succ]
-  rw [Set.subset_def]
-  intro L hL_mem
-  simp? [ComplexityClass.polyExistentiallyQuantify]
-  -- TODO if x ∈ Πᴾ n, then the language of pairs (x, ∅) is in Πᴾ n
-  use 0
-  sorry
-
-/--
-Pi n contained in Pi n+1
--/
-lemma PiPolyTimeHierarchy_subset_PiPolyTimeHierarchy_succ
-    (n : ℕ) : (Πᴾ n) ⊆ Πᴾ (n + 1) := by
-  sorry
-
-/--
-Sigma n contained in Pi n+1
--/
-lemma SigmaPolyTimeHierarchy_subset_PiPolyTimeHierarchy_succ
-    (n : ℕ) : (Σᴾ n) ⊆ Πᴾ (n + 1) := by
-  sorry
-
-/--
-Sigma n contained in Sigma n+1
--/
-lemma SigmaPolyTimeHierarchy_subset_SigmaPolyTimeHierarchy_succ
-    (n : ℕ) : (Σᴾ n) ⊆ Σᴾ (n + 1) := by
-  sorry
-
-lemma PolyTimeHierarchy_eq_union_sigma :
-  PolyTimeHierarchy = ⋃ n : ℕ, Σᴾ n := by
-  ext L
-  simp [PolyTimeHierarchy]
-
-lemma PolyTimeHierarchy_eq_union_pi :
-  PolyTimeHierarchy = ⋃ n : ℕ, Πᴾ n := by
-  sorry
+notation "∃ᴾ " C => polyExistentiallyQuantify C
 
 /--
 The class NP is the set of decision problems
@@ -188,12 +165,208 @@ such that the Turing machine accepts the pair `(x,w)`.
 
 See Definition 2.1 in Arora-Barak (2009).
 -/
-def NP : ComplexityClass := Σᴾ 1
+def NP : ComplexityClass := ∃ᴾ P
 
 /--
 The class coNP is the set of decision problems
 whose complements are in NP.
 -/
-def coNP : ComplexityClass := Πᴾ 1
+def coNP : ComplexityClass := ∀ᴾ P
+
+@[simp]
+lemma polyUniversallyQuantify_complement
+    (C : ComplexityClass) :
+    (∀ᴾ C).complement = ∃ᴾ (C.complement) := by
+  unfold complement polyExistentiallyQuantify polyUniversallyQuantify
+  ext L
+  simp only [Set.mem_setOf_eq]
+  refine exists_congr ?_
+  intro p
+  constructor
+  · intro ⟨L', hL', h_eq⟩
+    replace h_eq : Lᶜᶜ = BitstringDecisionProblem.existentiallyQuantifyOverPolynomial p L'ᶜ := by
+      rw [h_eq]
+      simp
+    use L'ᶜ
+    simp only [compl_compl] at *
+    exact And.symm ⟨h_eq, hL'⟩
+  · intro ⟨L', hL', h_eq⟩
+    replace h_eq : Lᶜ = BitstringDecisionProblem.universallyQuantifyOverPolynomial p L'ᶜ := by
+      rw [h_eq]
+      simp
+    use L'ᶜ
+
+@[simp]
+lemma polyExistentiallyQuantify_complement
+    (C : ComplexityClass) :
+    (∃ᴾ C).complement = ∀ᴾ (C.complement) := by
+  unfold complement polyExistentiallyQuantify polyUniversallyQuantify
+  ext L
+  simp only [Set.mem_setOf_eq]
+  refine exists_congr ?_
+  intro p
+  constructor
+  · intro ⟨L', hL', h_eq⟩
+    replace h_eq : Lᶜᶜ = BitstringDecisionProblem.universallyQuantifyOverPolynomial p L'ᶜ := by
+      rw [h_eq]
+      simp
+    use L'ᶜ
+    simp only [compl_compl] at *
+    exact And.symm ⟨h_eq, hL'⟩
+  · intro ⟨L', hL', h_eq⟩
+    replace h_eq : Lᶜ = BitstringDecisionProblem.existentiallyQuantifyOverPolynomial p L'ᶜ := by
+      rw [h_eq]
+      simp
+    use L'ᶜ
+
+lemma complement_mono
+    {C D : ComplexityClass} (h : C ⊆ D) :
+    C.complement ⊆ D.complement := by
+  unfold complement
+  simp only [Set.subset_def]
+  intro L hL
+  simp only [Set.mem_setOf_eq] at hL ⊢
+  exact h hL
+
+lemma complement_mono_iff
+    {C D : ComplexityClass} :
+    C ⊆ D ↔ C.complement ⊆ D.complement := by
+  constructor
+  · exact complement_mono
+  · intro h
+    have h' := complement_mono h
+    rw [complement_complement C, complement_complement D] at h'
+    exact h'
+
+lemma polyUniversallyQuantify_mono
+    {C D : ComplexityClass} (h : C ⊆ D) :
+    (∀ᴾ C) ⊆ (∀ᴾ D) := by
+  unfold polyUniversallyQuantify
+  simp only [Set.subset_def]
+  intro L hL
+  rcases hL with ⟨p, L', hL', h_eq⟩
+  use p
+  use L'
+  exact ⟨h hL', h_eq⟩
+
+lemma polyExistentiallyQuantify_mono
+    {C D : ComplexityClass} (h : C ⊆ D) :
+    (∃ᴾ C) ⊆ (∃ᴾ D) := by
+  unfold polyExistentiallyQuantify
+  simp only [Set.subset_def]
+  intro L hL
+  rcases hL with ⟨p, L', hL', h_eq⟩
+  use p
+  use L'
+  exact ⟨h hL', h_eq⟩
+
+
+lemma P_subset_NP : P ⊆ NP := by
+  unfold NP
+  sorry
+
+lemma P_subset_coNP : P ⊆ coNP := by
+  unfold coNP
+  rw [complement_mono_iff]
+  simp only [P_complement, polyUniversallyQuantify_complement]
+  exact P_subset_NP
+
+/--
+The Sigma levels of the polynomial time hierarchy.
+
+Σᴾ 0 = P
+Σᴾ n+1 = ∃ᴾ (Σᴾ n).complement
+-/
+def SigmaPolyTimeHierarchy : ℕ → ComplexityClass
+  | 0     => P
+  | n + 1 => (SigmaPolyTimeHierarchy n).complement.polyExistentiallyQuantify
+
+/--
+The Pi levels of the polynomial time hierarchy, defined as the complement of the Sigma levels.
+
+Πᴾ n = (Σᴾ n).complement
+-/
+def PiPolyTimeHierarchy (n : ℕ) : ComplexityClass :=
+  (SigmaPolyTimeHierarchy n).complement
+
+-- TODO bind more tightly
+scoped notation "Σᴾ" => SigmaPolyTimeHierarchy
+scoped notation "Πᴾ" => PiPolyTimeHierarchy
+
+def PolyTimeHierarchy : ComplexityClass :=
+  { L | ∃ n : ℕ, L ∈ Σᴾ n }
+
+@[simp]
+lemma SigmaPolyTimeHierarchy_zero : Σᴾ 0 = P := rfl
+
+@[simp]
+lemma PiPolyTimeHierarchy_zero : Πᴾ 0 = P.complement := rfl
+
+@[simp]
+lemma SigmaPolyTimeHierarchy_one : Σᴾ 1 = NP := by
+  simp [SigmaPolyTimeHierarchy, NP]
+
+@[simp]
+lemma PiPolyTimeHierarchy_one : Πᴾ 1 = coNP := by
+  simp [PiPolyTimeHierarchy, coNP, NP]
+
+lemma SigmaPolyTimeHierarchy_succ
+    (n : ℕ) : Σᴾ (n + 1) = ∃ᴾ (Πᴾ n) := by
+  simp only [SigmaPolyTimeHierarchy, PiPolyTimeHierarchy]
+
+lemma PiPolyTimeHierarchy_succ
+    (n : ℕ) : Πᴾ (n + 1) = ∀ᴾ (Σᴾ n) := by
+  simp only [PiPolyTimeHierarchy, SigmaPolyTimeHierarchy,
+    complement_complement,
+    polyExistentiallyQuantify_complement]
+
+@[simp]
+lemma PiPolyTimeHierarchy_complement
+    (n : ℕ) : (Πᴾ n).complement = Σᴾ n := by
+  simp [PiPolyTimeHierarchy, complement_complement]
+
+@[simp]
+lemma SigmaPolyTimeHierarchy_complement
+    (n : ℕ) : (Σᴾ n).complement = Πᴾ n := by
+  simp [PiPolyTimeHierarchy]
+
+private lemma PolyTimeHierarchy_subset_aux (n : ℕ) :
+    (Πᴾ n) ⊆ (Πᴾ (n + 1)) ∧ (Σᴾ n) ⊆ Σᴾ (n + 1) := by
+  induction n with
+  | zero =>
+    simp only [SigmaPolyTimeHierarchy_succ, PiPolyTimeHierarchy_succ]
+    simp only [PiPolyTimeHierarchy_zero, P_complement, SigmaPolyTimeHierarchy_zero]
+    constructor
+    · exact P_subset_coNP
+    · exact P_subset_NP
+  | succ n ih =>
+    simp only [SigmaPolyTimeHierarchy_succ, PiPolyTimeHierarchy_succ] at *
+    obtain ⟨ih_pi, ih_sigma⟩ := ih
+    constructor
+    · apply polyUniversallyQuantify_mono
+      exact ih_sigma
+    · apply polyExistentiallyQuantify_mono
+      exact ih_pi
+
+/--
+Pi n contained in Pi n+1
+-/
+lemma PiPolyTimeHierarchy_subset_PiPolyTimeHierarchy_succ
+    (n : ℕ) : (Πᴾ n) ⊆ Πᴾ (n + 1) := by
+  exact (PolyTimeHierarchy_subset_aux n).1
+
+/--
+Sigma n contained in Sigma n+1
+-/
+lemma SigmaPolyTimeHierarchy_subset_SigmaPolyTimeHierarchy_succ
+    (n : ℕ) : (Σᴾ n) ⊆ Σᴾ (n + 1) := by
+  exact (PolyTimeHierarchy_subset_aux n).2
+
+lemma PolyTimeHierarchy_eq_union_sigma :
+  PolyTimeHierarchy = ⋃ n : ℕ, Σᴾ n := by
+  ext L
+  simp [PolyTimeHierarchy]
+
+end ComplexityClass
 
 end ComplexityTheory
--- Cslib/Foundations/Data/Encoding.lean
+++ Cslib/Foundations/Data/Encoding.lean
@@ -20,63 +20,6 @@ def finEncodingListBool : Computability.FinEncoding (List Bool) where
   decode_encode _ := rfl
   ΓFin := Bool.fintype
 
-@[simp]
-lemma splitOnP_isNone_map_some {α : Type} (l : List α) :
-    List.splitOnP Option.isNone (l.map some) = [l.map some] := by
-  induction l with
-  | nil => rfl
-  | cons hd tl ih =>
-    simp [ih]
-
-@[simp]
-lemma splitOnP_append_cons {α : Type} (l1 l2 : List α)
-    (a : α) (P : α → Bool) (hPa : P a = true) :
-    List.splitOnP P (l1 ++ a :: l2)
-    = List.splitOnP P l1 ++ List.splitOnP P l2 := by
-  induction l1 with
-  | nil => simp [hPa]
-  | cons hd tl ih =>
-    obtain ⟨hd1, tl1, h1'⟩ := List.exists_cons_of_ne_nil (List.splitOnP_ne_nil P tl)
-    by_cases hPh : P hd <;> simp [*]
-
-@[simp]
-lemma getLeft?_comp_inl {α β : Type*} :
-    Sum.getLeft? ∘ @Sum.inl α β = Option.some := by
-  ext
-  simp
-
-@[simp]
-lemma getLeft?_comp_inr {α β : Type*} :
-    Sum.getLeft? ∘ @Sum.inr α β = fun x ↦ Option.none := by
-  ext
-  simp
-
-@[simp]
-lemma getRight?_comp_inl {α β : Type*} :
-    Sum.getRight? ∘ @Sum.inl α β = fun x ↦ Option.none := by
-  ext
-  simp
-
-@[simp]
-lemma getRight?_comp_inr {α β : Type*} :
-    Sum.getRight? ∘ @Sum.inr α β = Option.some := by
-  ext
-  simp
-
-@[simp]
-lemma List.filterMap_none {α β : Type*} (l : List α) :
-    l.filterMap (fun _ ↦ @Option.none β) = [] := by
-  induction l <;> simp [*]
-
-def finEncodingPair {α β : Type*} (ea : FinEncoding α) (eb : FinEncoding β) :
-    Computability.FinEncoding (α × β) where
-  Γ := ea.Γ ⊕ eb.Γ
-  encode x := (ea.encode x.1).map .inl ++ (eb.encode x.2).map .inr
-  decode x := Option.map₂ Prod.mk (ea.decode (x.filterMap Sum.getLeft?))
-      (eb.decode (x.filterMap Sum.getRight?))
-  decode_encode x := by
-    simp [List.filterMap_append, ea.decode_encode, eb.decode_encode]
-  ΓFin := inferInstance
 
 def encode_list_bool_prod_list_bool :
     (List Bool × List Bool) → List (Bool) :=
@@ -84,18 +27,12 @@ def encode_list_bool_prod_list_bool :
     | some b => [true, b]
     | none   => [false, false])
 
-def finEncodingListBoolProdListBool : Computability.FinEncoding (List Bool × List Bool) where
-  Γ := Option Bool
-  encode := fun ⟨l1, l2⟩ ↦ (l1.map some) ++ [none] ++ (l2.map some)
-  decode := fun l ↦
-    match l.splitOnP Option.isNone with
-    | [l1, l2] => some (l1.map (Option.getD · false), l2.map (Option.getD · false))
-    | _ => none
-  decode_encode := by
-    intro (l1, l2)
-    simp
-  ΓFin := instFintypeOption
+def decode_list_bool_prod_list_bool :
+    List (Bool) → Option (List Bool × List Bool) := sorry
 
+lemma decode_encode_list_bool_prod_list_bool :
+    ∀ x : List Bool × List Bool,
+      decode_list_bool_prod_list_bool (encode_list_bool_prod_list_bool x) = some x := sorry
 
 
 end Encodings
