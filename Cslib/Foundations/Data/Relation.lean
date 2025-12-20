e2ddc61ea88e2b066185c6ea55a428bb33cdd1e0
Chris Henson <chrishenson.net@gmail.com>
2025-12-19 23:40:37 -0600

    naming

--- Cslib/Foundations/Data/Relation.lean
+++ Cslib/Foundations/Data/Relation.lean
@@ -257,22 +257,22 @@ theorem StronglyConfluent.toConfluent (h : StronglyConfluent r) : Confluent r :=
 variable {r₁ r₂ : α → α → Prop}
 
 @[scoped grind <=]
-theorem extend_inl (r₁_ab : r₁ a b) : (r₁ ⊔ r₂) a b :=
+theorem join_inl (r₁_ab : r₁ a b) : (r₁ ⊔ r₂) a b :=
   Or.inl r₁_ab
 
 @[scoped grind <=]
-theorem extend_inr (r₂_ab : r₂ a b) : (r₁ ⊔ r₂) a b :=
+theorem join_inr (r₂_ab : r₂ a b) : (r₁ ⊔ r₂) a b :=
   Or.inr r₂_ab
 
 @[scoped grind <=]
-theorem extend_inl_reflTransGen (r₁_ab : ReflTransGen r₁ a b) : ReflTransGen (r₁ ⊔ r₂) a b := by
+theorem join_inl_reflTransGen (r₁_ab : ReflTransGen r₁ a b) : ReflTransGen (r₁ ⊔ r₂) a b := by
   induction r₁_ab <;> grind
 
 @[scoped grind <=]
-theorem extend_inr_reflTransGen (r₂_ab : ReflTransGen r₂ a b) : ReflTransGen (r₁ ⊔ r₂) a b := by
+theorem join_inr_reflTransGen (r₂_ab : ReflTransGen r₂ a b) : ReflTransGen (r₁ ⊔ r₂) a b := by
   induction r₂_ab <;> grind
 
-lemma Commute.union_left (c₁ : Commute r₁ r₃) (c₂ : Commute r₂ r₃) : Commute (r₁ ⊔ r₂) r₃ := by
+lemma Commute.join_left (c₁ : Commute r₁ r₃) (c₂ : Commute r₂ r₃) : Commute (r₁ ⊔ r₂) r₃ := by
   intro x y z xy xz
   induction xy with
   | refl => grind
@@ -286,13 +286,13 @@ lemma Commute.union_left (c₁ : Commute r₁ r₃) (c₂ : Commute r₂ r₃) :
       obtain ⟨_, _, _⟩ := c₂ (.single bc) bw
       grind [ReflTransGen.trans]
 
-theorem Commute.union_Confluent (c₁ : Confluent r₁) (c₂ : Confluent r₂) (comm : Commute r₁ r₂) :
+theorem Commute.join_confluent (c₁ : Confluent r₁) (c₂ : Confluent r₂) (comm : Commute r₁ r₂) :
     Confluent (r₁ ⊔ r₂) := by
   intro a b c ab ac
   induction ab generalizing c with
   | refl => exists c
   | @tail x y ax xy ih =>
-    have h_comm : Commute (r₁ ⊔ r₂) (r₁ ⊔ r₂) := by apply_rules [union_left, symmetric]
+    have h_comm : Commute (r₁ ⊔ r₂) (r₁ ⊔ r₂) := by apply_rules [join_left, symmetric]
     obtain ⟨z, xz, cz⟩ := ih ac
     obtain ⟨w, yw, zw⟩ := h_comm (.single xy) xz
     exact ⟨w, yw, cz.trans zw⟩
