7b4c4435f2ce9c9868ac20b05dcc7d49821b30c8
Chris Henson <chrishenson.net@gmail.com>
2025-12-23 03:33:52 -0600

    revert chnages related to NA.Run

--- Cslib/Computability/Automata/NA/Hist.lean
+++ Cslib/Computability/Automata/NA/Hist.lean
@@ -50,6 +50,9 @@ which projects back onto `ss`. -/
 theorem hist_run_exists {xs : ωSequence Symbol} {ss : ωSequence State}
     (h_run : na.Run xs ss) : ∃ ss', (na.addHist start' tr').Run xs ss' ∧ ss'.map fst = ss := by
   use ⟨fun n ↦ (ss n, makeHist start' tr' xs ss n)⟩
-  grind
+  constructor
+  · simp only [addHist]
+    grind [Run]
+  · grind
 
 end Cslib.Automata.NA
