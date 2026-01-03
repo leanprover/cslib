15e9b1daa0b76f57a21c4b53bc793789bf71d2fd
Chris Henson <chrishenson.net@gmail.com>
2026-01-03 11:05:48 -0600

    feat: move to the module system

--- Cslib/Languages/LambdaCalculus/LocallyNameless/Fsub/Safety.lean
+++ Cslib/Languages/LambdaCalculus/LocallyNameless/Fsub/Safety.lean
@@ -4,7 +4,11 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Chris Henson
 -/
 
-import Cslib.Languages.LambdaCalculus.LocallyNameless.Fsub.Typing
+module
+
+public import Cslib.Languages.LambdaCalculus.LocallyNameless.Fsub.Typing
+
+@[expose] public section
 
 /-! # λ-calculus
 
