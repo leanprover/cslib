b8a48f023b3298f3aa6c62aa2373df9fc2940220
Chris Henson <chrishenson.net@gmail.com>
2026-01-21 20:49:50 -0500

    naming, a repeated theorem

--- Cslib/Foundations/Data/Relation.lean
+++ Cslib/Foundations/Data/Relation.lean
@@ -45,10 +45,10 @@ theorem TransGen.to_eqvGen (h : TransGen r a b) : EqvGen r a b := by
 theorem ReflTransGen.to_eqvGen (h : ReflTransGen r a b) : EqvGen r a b := by
   induction h <;> grind
 
-theorem CompRel.to_eqvGen (h : CompRel r a b) : EqvGen r a b := by
+theorem _root_.CompRel.to_eqvGen (h : CompRel r a b) : EqvGen r a b := by
   induction h <;> grind
 
-attribute [scoped grind →] ReflGen.to_eqvGen TransGen.to_eqvGen ReflTransGen.to_eqvGen 
+attribute [scoped grind →] ReflGen.to_eqvGen TransGen.to_eqvGen ReflTransGen.to_eqvGen
   CompRel.to_eqvGen
 
 /-- The relation `r` 'up to' the relation `s`. -/
@@ -312,40 +312,29 @@ theorem reflTransGen_mono_closed (h₁ : Subrelation r₁ r₂) (h₂ : Subrelat
   ext
   exact ⟨ReflTransGen.mono @h₁, reflTransGen_closed @h₂⟩
 
-lemma ReflGen.compRel_symm :
-  ReflGen (CompRel r) a b → ReflGen (CompRel r) b a
+lemma ReflGen.compRel_symm : ReflGen (CompRel r) a b → ReflGen (CompRel r) b a
 | .refl => .refl
-| .single h =>
-    match h with
-    | Or.inl h' => .single (Or.inr h')
-    | Or.inr h' => .single (Or.inl h')
+| .single (.inl h) => .single (.inr h)
+| .single (.inr h) => .single (.inl h)
 
-lemma ReflTransGen.compRel_swap (h : ReflTransGen (CompRel r) a b) : 
-    ReflTransGen (CompRel (Function.swap r)) b a := by
-  induction h with
-  | refl => exact .refl
-  | tail _ hbc ih => 
-    exact ih.head (match hbc with
-     | Or.inl h => Or.inl h
-     | Or.inr h => Or.inr h)
-
-theorem EqvGen_eq_reflTransGen_compRel : EqvGen r = ReflTransGen (CompRel r) := by
+@[simp, grind =]
+theorem EqvGen_eq_reflTransGen_compRel : ReflTransGen (CompRel r) = EqvGen r := by
   ext a b
   constructor
-  · intro h
-    induction h with
-    | rel _ _ ih => exact .single (.inl ih)
-    | refl x => exact .refl
-    | symm x y eq ih =>
-      rw [← compRel_swap]
-      exact ih.compRel_swap      
-    | trans _ _ _ _ _ ih₁ ih₂ => exact ih₁.trans ih₂
   · intro h
     induction h with
     | refl => exact .refl _
     | tail hab hbc ih =>
       cases hbc with
       | inl h => exact ih.trans _ _ _ (.rel _ _ h)
-      | inr h => exact ih.trans _ _ _ (.symm _ _ (EqvGen.rel _ _ h))
+      | inr h => exact ih.trans _ _ _ (.symm _ _ (.rel _ _ h))
+  · intro h
+    induction h with
+    | rel _ _ ih => exact .single (.inl ih)
+    | refl x => exact .refl
+    | symm x y eq ih =>
+      rw [compRel_swap]
+      exact reflTransGen_swap.mp ih
+    | trans _ _ _ _ _ ih₁ ih₂ => exact ih₁.trans ih₂
 
 end Relation
