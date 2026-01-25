db100ea2101d8dc5088cadad5ea359bfc8f31fa3
Shreyas <shreyasss94@gmail.com>
2026-01-25 05:45:05 +0100

    Improve docs

--- Cslib/Algorithms/QueryModel.lean
+++ Cslib/Algorithms/QueryModel.lean
@@ -22,21 +22,25 @@ parametric type of query operations.
 ## Main definitions
 
 - `PureCosts`  : A typeclass that every model needs to log or cost pure monadic operations
-- `Model Q` : A model type for a query type `Q : Type u → Type u`
+- `Model Q c` : A model type for a query type `Q : Type u → Type u` and cost type `c`
 - `Prog Q α` : The type of programs of query type `Q` and return type `α`. This is a free monad
       under the hood
 - `eval`, `time` : concrete execution semantics of a `Prog Q α` for a given model of `Q`
 
 
-## Important notes
+## How to set up an algorithm
 
 This model is a lightweight framework for specifying and verifying both the correctness
 and complexity of algorithms in lean. To specify an algorithm, one must:
-1. Define an inductive type of queries which carries. This type must have one parameter `α` and
-  one index type. The parameter type `α` is the return type of an
-  which is the type supplied to the query and an index which denotes the output type of each query
-
-2. C
+1. Define an inductive type of queries which carries. This type must at least one index parameter
+  which denotes the output type of the query. Additionally it helps to have a parameter `α` on which
+  the index type depends. This way, any instance parameters of `α` can be used easily
+  for the output types. The signatures of `Model.evalQuery` and `Model.Cost` are fixed.
+  So you can't supply instances for the index type there.
+2. Define one or more cost types `C` and instances of `PureCosts` for this cost type.
+3. Define a `Model Q C` type instance
+4. Write your algorithm as a monadic program in `Prog Q α`. With sufficient type anotations
+  each query `q : Q` is automatically lifted into `Prog Q α`.
 ## Tags
 
 query model, free monad, time complexity, Prog
@@ -123,8 +127,6 @@ structure Reduction (Q Q' : Type u → Type u) where
 
 
 end Reduction
-
-
 
 end Prog
 
