58900bdbd89ef01f2ea4007dcb3eb22ae2bb68ad
David Wegmann <david.wegmann@fau.de>
2026-03-09 12:05:48 +0100

    added license and author

--- Cslib/Languages/LambdaCalculus/LocallyNameless/Untyped/MultiSubst.lean
+++ Cslib/Languages/LambdaCalculus/LocallyNameless/Untyped/MultiSubst.lean
@@ -1,3 +1,10 @@
+/-
+Copyright (c) 2025 David Wegmann. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: David Wegmann
+-/
+
+
 module
 
 public import Cslib.Foundations.Data.Relation
