e5858155978d1727ba04b053556dffc78db7e8e9
Maximiliano <maxonomar@ciencias.unam.mx>
2026-03-13 14:22:04 -0600

    fix typo

--- Cslib/Languages/LambdaCalculus/LocallyNameless/Untyped/FullEta.lean
+++ Cslib/Languages/LambdaCalculus/LocallyNameless/Untyped/FullEta.lean
@@ -83,7 +83,7 @@ lemma close_eta_steps (hx_M : x ∉ M.fv) (st_M : ReflGen FullEta (M ^ fvar x) N
     ReflGen FullEta M.abs (N ^* x).abs := by
   cases st_M with
   | refl =>
-    have eq_M : M = (M ^ fvar x) ^* x := open_close x M 0 hx_M
+    have eq_M : M = (M ^ fvar x) ^* x := open_close_var x M hx_M
     rw [←eq_M]
   | single st =>
     exact .single (.abs {x} fun _ _ => by grind)
