242fb101a33896f094d762658c14215d47efa5d1
Maximiliano Onofre Martínez <maxonomar@ciencias.unam.mx>
2026-03-13 14:48:20 -0600

    feat: prove strong confluence of eta-reduction (#425)
    This PR formalizes the strong confluence of full η-reduction
    (`stronglyConfluent_eta`) for the locally nameless untyped λ-calculus.
    
    ---------
    
    Co-authored-by: Chris Henson <46805207+chenson2018@users.noreply.github.com>
    Co-authored-by: Chris Henson <chrishenson.net@gmail.com>

--- Cslib/Languages/LambdaCalculus/LocallyNameless/Untyped/FullEta.lean
+++ Cslib/Languages/LambdaCalculus/LocallyNameless/Untyped/FullEta.lean
@@ -37,13 +37,22 @@ namespace FullEta
 
 attribute [scoped grind .] appL appR
 
-variable {M M' : Term Var}
+variable {M M' N N' : Term Var}
 
 /-- The right side of an η-reduction is locally closed. -/
+@[scoped grind →]
 lemma step_lc_r (step : M ⭢ηᶠ M') : LC M' := by
   induction step
   case abs => constructor; assumption
   all_goals grind
+
+/- Single reduction `app M (fvar x) ⭢ηᶠ N` implies `N = app M' (fvar x)` for some M' -/
+@[scoped grind →]
+lemma invert_step_app_fvar (step : (app M (fvar x)) ⭢ηᶠ N) :
+    ∃ M', N = app M' (fvar x) ∧ M ⭢ηᶠ M' := by
+  cases step with
+  | appR _ step_M => exact ⟨_, rfl, step_M⟩
+  | appL _ step_x => cases step_x
 
 variable [HasFresh Var] [DecidableEq Var]
 
@@ -55,6 +64,27 @@ lemma step_not_fv (step : M ⭢ηᶠ M') (hw : w ∉ M.fv) : w ∉ M'.fv := by
     have := open_close x
     grind [close_preserve_not_fvar, open_fresh_preserve_not_fvar]
   | _ => grind
+
+/-- Substitution of a fresh variable preserves an η-reduction step. -/
+@[scoped grind ←]
+lemma eta_subst_fvar {x y : Var} (step : M ⭢ηᶠ M') : M [ x := fvar y ] ⭢ηᶠ M' [ x := fvar y ] := by
+  have lc_fy : LC (fvar y) := by constructor
+  induction step with
+  | eta lc_M => exact eta (subst_lc lc_M lc_fy)
+  | appL lc_Z _ ih => exact appL (subst_lc lc_Z lc_fy) ih
+  | appR lc_Z _ ih => exact appR (subst_lc lc_Z lc_fy) ih
+  | abs xs _ ih =>
+    apply abs (xs ∪ {x, y})
+    grind
+
+/- Closing a sequence of η-reduction steps over a fresh variable preserves the steps. -/
+open Relation in
+lemma close_eta_steps (hx_M : x ∉ M.fv) (st_M : ReflGen FullEta (M ^ fvar x) N) :
+    ReflGen FullEta M.abs (N ^* x).abs := by
+  cases st_M with
+  | refl => rw [←open_close_var x M hx_M]
+  | single st =>
+    exact .single (.abs {x} fun _ _ => by grind)
 
 end LambdaCalculus.LocallyNameless.Untyped.Term.FullEta
 
