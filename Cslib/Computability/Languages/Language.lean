c0ace9d8005d909b0a9e8acd01b0ed1b87023220
Chris Henson <chrishenson.net@gmail.com>
2026-04-02 11:23:05 +0200

    sub_one_mul grind regressions

--- Cslib/Computability/Languages/Language.lean
+++ Cslib/Computability/Languages/Language.lean
@@ -49,17 +49,23 @@ theorem mem_sub_one (x : List α) : x ∈ (l - 1) ↔ x ∈ l ∧ x ≠ [] :=
 theorem reverse_sub (l m : Language α) : (l - m).reverse = l.reverse - m.reverse := by
   ext x; simp [mem_sub]
 
+#adaptation_note
+/-- A grind regression found moving to nightly-2026-03-31 (changes from lean#13166) -/
 @[scoped grind =]
 theorem sub_one_mul : (l - 1) * l = l * l - 1 := by
   ext x; constructor
   · rintro ⟨u, h_u, v, h_v, rfl⟩
     constructor
-    · refine ⟨u, ?_, v, ?_⟩ <;> grind
-    · grind [append_eq_nil_iff, mem_one]
+    · exact ⟨u, Set.mem_of_mem_inter_left h_u, v, h_v, rfl⟩
+    · by_contra h
+      have := mem_sub_one u |>.mp h_u
+      have := mem_one (u ++ v) |>.mp h
+      grind [append_eq_nil_iff]
   · rintro ⟨⟨u, h_u, v, h_v, rfl⟩, h_x⟩
     rcases eq_or_ne u [] with (rfl | h_u')
-    · refine ⟨v, ?_, [], ?_⟩ <;> grind [mem_sub, mem_one]
-    · refine ⟨u, ?_, v, ?_⟩ <;> grind
+    · use v, (mem_sub l 1 v |>.mpr) ⟨h_v, Not.intro h_x⟩, []
+      grind [mem_sub, mem_one]
+    · use u, (mem_sub_one u).mpr ⟨h_u, h_u'⟩, v
 
 @[scoped grind =]
 theorem mul_sub_one : l * (l - 1) = l * l - 1 := by
