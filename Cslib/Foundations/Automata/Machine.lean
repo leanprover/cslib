3c90e9f453450532f6d939a054a4317def546f69
Matt Hunzinger <matt@hunzinger.me>
2026-04-09 21:23:45 -0400

    refactor: change from Stream' to ωSequence

--- Cslib/Foundations/Automata/Machine.lean
+++ Cslib/Foundations/Automata/Machine.lean
@@ -6,9 +6,9 @@ Authors: Matt Hunzinger
 
 module
 
+public import Cslib.Foundations.Data.OmegaSequence.Init
 public import Mathlib.CategoryTheory.Monoidal.Braided.Basic
 public import Mathlib.CategoryTheory.Monoidal.Closed.Basic
-public import Mathlib.Data.Stream.Init
 
 @[expose] public section
 
@@ -20,7 +20,7 @@ public import Mathlib.Data.Stream.Init
 
 -/
 
-namespace CsLib.Automata
+namespace Cslib.Automata
 
 universe u
 
@@ -32,9 +32,9 @@ namespace Machine
 variable {X Y Z : Machine.{u}}
 
 /-- Homomorphism as a stream function. -/
-def Hom (X Y : Machine.{u}) := Stream' X → Stream' Y
+def Hom (X Y : Machine.{u}) := ωSequence X → ωSequence Y
 
-def id (X : Machine.{u}) : Stream' X → Stream' X := fun x => x
+def id (X : Machine.{u}) : ωSequence X → ωSequence X := fun x => x
 
 def comp (f : Hom X Y) (g : Hom Y Z) := g ∘ f
 
@@ -51,11 +51,11 @@ def tensorHom
     {X₁ Y₁ X₂ Y₂ : Machine.{u}}
     (f : X₁ ⟶ Y₁)
     (g : X₂ ⟶ Y₂)
-    (v : Stream' (X₁ × X₂)) :
-    Stream' (Y₁ × Y₂) :=
-  let x₁s := f (Stream'.map (fun p => p.1) v)
-  let x₂s := g (Stream'.map (fun p => p.2) v)
-  Stream'.zip Prod.mk x₁s x₂s
+    (v : ωSequence (X₁ × X₂)) :
+    ωSequence (Y₁ × Y₂) :=
+  let x₁s := f (ωSequence.map (fun p => p.1) v)
+  let x₂s := g (ωSequence.map (fun p => p.2) v)
+  ωSequence.zip Prod.mk x₁s x₂s
 
 def whiskerLeft
     (X : Machine.{u}) {Y₁ Y₂ : Machine.{u}} : (Y₁ ⟶ Y₂) → (tensorObj X Y₁ ⟶ tensorObj X Y₂) :=
@@ -69,11 +69,11 @@ def tensorUnit : Machine := PUnit
 
 section associators
 
-def associator_hom : Stream' ((X × Y) × Z) → Stream' (X × (Y × Z)) :=
-  Stream'.map fun ((a, b), c) => (a, (b, c))
+def associator_hom : ωSequence ((X × Y) × Z) → ωSequence (X × (Y × Z)) :=
+  ωSequence.map fun ((a, b), c) => (a, (b, c))
 
-def associator_inv : Stream' (X × (Y × Z)) → Stream' ((X × Y) × Z) :=
-  Stream'.map fun (a, (b, c)) => ((a, b), c)
+def associator_inv : ωSequence (X × (Y × Z)) → ωSequence ((X × Y) × Z) :=
+  ωSequence.map fun (a, (b, c)) => ((a, b), c)
 
 theorem associator_hom_inv : associator_hom ≫ associator_inv = 𝟙 (tensorObj (tensorObj X Y) Z) :=
   rfl
@@ -90,9 +90,9 @@ def associator
 
 end associators
 
-def leftUnitor_hom : Stream' (PUnit × X) → Stream' X := Stream'.map Prod.snd
+def leftUnitor_hom : ωSequence (PUnit × X) → ωSequence X := ωSequence.map Prod.snd
 
-def leftUnitor_inv : Stream' X → Stream' (PUnit × X) := Stream'.map fun x => (PUnit.unit, x)
+def leftUnitor_inv : ωSequence X → ωSequence (PUnit × X) := ωSequence.map fun x => (PUnit.unit, x)
 
 theorem leftUnitor_hom_inv_id : leftUnitor_hom ≫ leftUnitor_inv = 𝟙 (tensorObj tensorUnit X) := rfl
 
@@ -104,9 +104,9 @@ def leftUnitor (X : Machine.{u}) : tensorObj tensorUnit X ≅ X where
   hom_inv_id := leftUnitor_hom_inv_id
   inv_hom_id := leftUnitor_inv_hom_id
 
-def rightUnitor_hom : Stream' (X × PUnit) → Stream' X := Stream'.map Prod.fst
+def rightUnitor_hom : ωSequence (X × PUnit) → ωSequence X := ωSequence.map Prod.fst
 
-def rightUnitor_inv : Stream' X → Stream' (X × PUnit) := Stream'.map fun x => (x, PUnit.unit)
+def rightUnitor_inv : ωSequence X → ωSequence (X × PUnit) := ωSequence.map fun x => (x, PUnit.unit)
 
 theorem rightUnitor_hom_inv_id : rightUnitor_hom ≫ rightUnitor_inv = 𝟙 (tensorObj X tensorUnit) :=
   rfl
@@ -131,7 +131,8 @@ instance : MonoidalCategory Machine where
 
 open MonoidalCategory
 
-def braiding_hom (X Y : Machine.{u}) : Stream' (X × Y) → Stream' (Y × X) := Stream'.map Prod.swap
+def braiding_hom (X Y : Machine.{u}) : ωSequence (X × Y) → ωSequence (Y × X) :=
+  ωSequence.map Prod.swap
 
 theorem braiding_hom_inv_id (X Y : Machine.{u}) : braiding_hom X Y ≫ braiding_hom Y X = 𝟙 (X ⊗ Y) :=
   rfl
@@ -146,14 +147,14 @@ instance : SymmetricCategory Machine where
   braiding
 
 def rightAdj {X : Machine.{u}} : Machine ⥤ Machine where
-  obj Y := Stream' X → Y
-  map f s n xs := f (fun m => s m xs) n
+  obj Y := ωSequence X → Y
+  map f s := ⟨fun n xs => f ⟨fun m => s m xs⟩ n⟩
 
-def toFun (f : (tensorLeft X).obj A ⟶ Y) (a : Stream' A) (n : ℕ) (xs : Stream' X) : Y :=
-  f (Stream'.zip Prod.mk xs a) n
+def toFun (f : (tensorLeft X).obj A ⟶ Y) (a : ωSequence A) : ωSequence (ωSequence X → Y) :=
+  ⟨fun n xs => f (ωSequence.zip Prod.mk xs a) n⟩
 
-def invFun (g : Z ⟶ X.rightAdj.obj Y) (xas : Stream' (X × Z)) (n : ℕ) : Y :=
-  g (Stream'.map Prod.snd xas) n (Stream'.map Prod.fst xas)
+def invFun (g : Z ⟶ X.rightAdj.obj Y) (xas : ωSequence (X × Z)) : ωSequence Y :=
+  ⟨fun n => g (ωSequence.map Prod.snd xas) n (ωSequence.map Prod.fst xas)⟩
 
 theorem left_inv (f : (tensorLeft X).obj A ⟶ Y) : invFun (toFun f) = f := rfl
 
@@ -185,4 +186,4 @@ instance : MonoidalClosed Machine where
 
 end Machine
 
-end CsLib.Automata
+end Cslib.Automata
