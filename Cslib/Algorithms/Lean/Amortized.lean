c5740ee886bdacb527ba8cdce914588220932309
Simon Cruanes <simon.cruanes.2007@m4x.org>
2026-05-10 00:14:34 -0400

    wip: define physicist method in Amortized.lean, start using it for the queue

--- Cslib/Algorithms/Lean/Amortized.lean
+++ Cslib/Algorithms/Lean/Amortized.lean
@@ -1,4 +1,3 @@
-
 /-
 Copyright (c) 2026 Simon Cruanes. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
@@ -8,6 +7,7 @@ Authors: Simon Cruanes
 module
 
 import Cslib.Init
+public import Cslib.Algorithms.Lean.TimeM
 
 /-!
 # Amortized cost analysis
@@ -25,6 +25,18 @@ class Potential α where
   /-- non-negative potential. Initial potential should be 0.
       [Okasaki, *Purely Functional Data Structures*, 1996][okasaki1996] -/
   potential : α → Nat
+
+class Op α o where
+  applyOp : α → o → TimeM ℕ α
+
+@[simp] def applyOps {α o : Type u} [Op α o] (ops : List o) (x : α)
+    : TimeM ℕ α :=
+  List.foldlM (fun x op => Op.applyOp x op) x ops
+
+/-- Amortized cost with the physicist's method,
+    following Okasaki, chapter 5 -/
+def amortizedCost {α o : Type*} [Op α o] [Potential α] (x : α) (op : o) : ℕ :=
+  (Op.applyOp x op).time + Potential.potential (Op.applyOp x op).ret - Potential.potential x
 
 end Cslib.Algorithms.Lean.Amortized
 
