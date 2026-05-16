ce3adece5abdcd8c0fed49a7c70538e595cdaeb7
Fabrizio Montesi <famontesi@gmail.com>
2026-05-16 18:38:30 +0200

    NTM from NA

--- Cslib/Computability/Machines/SingleTapeTuring/NonDeterministic.lean
+++ Cslib/Computability/Machines/SingleTapeTuring/NonDeterministic.lean
@@ -10,7 +10,11 @@ public import Cslib.Computability.Automata.NA.Basic
 public import Cslib.Foundations.Data.BiTape
 public import Cslib.Foundations.Semantics.LTS.HasTau
 
-/-! # Nondeterministic Single-Tape Turing Machines -/
+/-! # Nondeterministic Single-Tape Turing Machines
+
+Nondeterministic Turing Machines (NTMs) defined as the composition of a nondeterministic controller
+(an `NA`) with a bidirectional tape (`BiTape`).
+-/
 
 @[expose] public section
 
@@ -21,36 +25,46 @@ open Automata Turing
 -- inductive Move
 --   | stay | left | right
 
+/-- The transition labels used by a controller. -/
 structure Controller.TrLabel (State Symbol : Type*) where
-  read : Option Symbol
+  read : Symbol
   write : Option Symbol
   move : Option Turing.Dir -- Might wanna use a larger inductive here (see Move)
 
+/-- The automaton that defines the behaviour of an NTM. -/
 structure Controller (State Symbol : Type*)
-    extends NA State (Controller.TrLabel State Symbol)
+    extends NA State (Controller.TrLabel State Symbol) where
 
+/-- Configuration of a Turing machine. -/
 structure Cfg (State Symbol : Type*) where
   state : State
   tape : Turing.BiTape Symbol
 
+/-- Single-tape Nondeterministic Turing Machine. -/
 structure SingleTapeNTM (State Symbol : Type*) where
-  /-- The controller automaton of the machine. -/
+  /-- The controller of the machine. -/
   controller : Controller State Symbol
   /-- The set of accepting (halting) states. -/
   accept : Set State
+  /-- Proof that all accepting states are halting states. -/
+  controller_halts (hmem : s ∈ accept) : ¬∃ μ s', controller.Tr s μ s'
 
--- Transition relation for configurations
--- Due to the way BiTape is written, Symbol cannot be universe-polymorphic. To be fixed.
-def SingleTapeNTM.Tr {Symbol} (m : SingleTapeNTM State Symbol)
+/-- The 'yields' relation, a binary relation on configurations that codifies an execution step. -/
+def SingleTapeNTM.Yields {Symbol} (m : SingleTapeNTM State Symbol)
     (c c' : Cfg State Symbol) : Prop :=
-  c.state ∉ m.accept ∧
-  ∃ μ, m.controller.Tr c.state μ c'.state ∧
-    μ.read = c.tape.head ∧ c'.tape = (c.tape.write μ.write).optionMove μ.move
+  ∃ μ, m.controller.Tr c.state μ c'.state ∧ -- The controller can perform the move
+    μ.read = c.tape.head ∧ -- The tape has the expected symbol to be read
+    c'.tape = (c.tape.write μ.write).optionMove μ.move -- Write effect on the tape
 
-def SingleTapeNTM.MTr {Symbol} (m : SingleTapeNTM State Symbol) := Relation.ReflTransGen m.Tr
+/-- The extended 'yields' relation, obtained by closing `Yields` for reflexivity and transitivity.
+-/
+def SingleTapeNTM.MYields {Symbol} (m : SingleTapeNTM State Symbol) :=
+  Relation.ReflTransGen m.Yields
 
-def SingleTapeNTM.Accepts {Symbol} (m : SingleTapeNTM State Symbol)
-    (xs : List Symbol) : Prop :=
-  ∃ s ∈ m.controller.start, ∃ c', m.MTr (Cfg.mk s (Turing.BiTape.mk₁ xs)) c'
+/-- An NTM is an acceptor of finite lists of symbols. -/
+@[simp, scoped grind =]
+instance {Symbol} : Acceptor (SingleTapeNTM State Symbol) Symbol where
+  Accepts (m : SingleTapeNTM State Symbol) (xs : List Symbol) :=
+    ∃ s ∈ m.controller.start, ∃ c', m.MYields {state := s, tape := Turing.BiTape.mk₁ xs} c'
 
 end Cslib.Computability.Turing.NTM
