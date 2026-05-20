98a097133740377bff9c2fe2fa4a39ffe17d6aad
lengyijun <sjtu5140809011@gmail.com>
2026-05-21 07:35:53 +0800

    feat: precise `open_preserve_not_fvar` `subst_preserve_not_fvar`

--- Cslib/Languages/LambdaCalculus/LocallyNameless/Untyped/Properties.lean
+++ Cslib/Languages/LambdaCalculus/LocallyNameless/Untyped/Properties.lean
@@ -56,13 +56,13 @@ lemma close_preserve_not_fvar {k y} (m : Term Var) : (m⟦k ↜ y⟧).fv = m.fv.
   induction m generalizing k <;> grind
 
 /-- Opening preserves free variables. -/
-lemma open_preserve_not_fvar {k x} (m n : Term Var) (nmem_m : x ∉ m.fv) (nmem_n : x ∉ n.fv) :
-    x ∉ (m⟦k ↝ n⟧).fv := by
+theorem open_preserve_not_fvar {k} {m n : Term Var} :
+    m⟦k ↝ n⟧.fv = m.fv ∪ n.fv ∨ m⟦k ↝ n⟧.fv = m.fv := by
   induction m generalizing k <;> grind
 
 /-- Substitution preserves free variables. -/
-lemma subst_preserve_not_fvar {x y : Var} (m n : Term Var) (nmem : x ∉ m.fv ∪ n.fv) :
-    x ∉ (m [y := n]).fv := by
+lemma subst_preserve_not_fvar {y : Var} (m n : Term Var) :
+    (m [y := n]).fv = (m.fv.erase y) \/ (m [y := n]).fv = (m.fv.erase y) ∪ n.fv:= by
   induction m <;> grind
 
 /-- Closing removes a free variable. -/
@@ -135,6 +135,7 @@ lemma open_close_to_subst (m : Term Var) (x y : Var) (k : ℕ) (m_lc : LC m) :
     m ⟦k ↜ x⟧⟦k ↝ fvar y⟧ = m [x := fvar y] := by
   induction m_lc generalizing k with
   | abs xs t =>
+    unfold closeRec openRec
     have ⟨x', _⟩ := fresh_exists <| free_union [fv] Var
     grind [
       swap_open, =_ swap_open_fvar_close,
