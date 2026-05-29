5087c872e1afdb8536b59f168d8e3b3ea50e778c
dhruvgupta-zetesis <dhruv@zetesislabs.com>
2026-05-29 17:20:18 +0530

    Re-run mk_all: merge mis-ordered VCDimension/VersionSpace in Cslib.lean

--- Cslib.lean
+++ Cslib.lean
@@ -139,6 +139,6 @@ public import Cslib.Logics.Modal.Denotation
 public import Cslib.Logics.Propositional.Defs
 public import Cslib.Logics.Propositional.NaturalDeduction.Basic
 public import Cslib.MachineLearning.PACLearning.Defs
-public import Cslib.MachineLearning.PACLearning.VersionSpace
 public import Cslib.MachineLearning.PACLearning.VCDimension
+public import Cslib.MachineLearning.PACLearning.VersionSpace
 public import Cslib.Probability.PMF
