0708becef48018093b2c7f66a957ee573d2cc6af
Fabrizio Montesi <fabrizio.montesi@pm.me>
2026-07-07 14:40:21 +0200

    dynamic logic modalities

--- Cslib/Foundations/Logic/Operators.lean
+++ Cslib/Foundations/Logic/Operators.lean
@@ -62,7 +62,7 @@ end Propositional
 
 section Modal
 
-/-! ## Modalities -/
+/-! ## Basic modalities -/
 
 /-- The type `α` has a box modality (`□`). -/
 class HasBox (α : Type*) where
@@ -79,6 +79,26 @@ class HasDiamond (α : Type*) where
 @[inherit_doc] scoped prefix:40 "◇" => HasDiamond.diamond
 
 end Modal
+
+section Dynamic
+
+/-! ## Dynamic modalities  -/
+
+/-- The type `α` has a dynamic box modality with action type `β` (`[a]φ`). -/
+class HasDynamicBox (α β : Type*) where
+  /-- `b` is necessarily valid after `a`. -/
+  dynBox (a : β) (b : α) : α
+
+@[inherit_doc] scoped notation "[" a "]" φ => HasDynamicBox.dynBox a φ
+
+/-- The type `α` has a dynamic diamond modality with action type `β` (`[a]φ`). -/
+class HasDynamicDiamond (α β : Type*) where
+  /-- `b` is possibly valid after `a`. -/
+  dynDiamond (a : β) (b : α) : α
+
+@[inherit_doc] scoped notation "⟨" a "⟩" φ => HasDynamicDiamond.dynDiamond a φ
+
+end Dynamic
 
 section Linear
 
