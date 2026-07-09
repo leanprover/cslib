624ddbd4a3fde6da9a777d7ad6114bafd38d03e1
Chris Henson <chrishenson.net@gmail.com>
2026-07-09 04:55:38 +0100

    split out branches of grind proofs

--- Cslib/Languages/LambdaCalculus/LocallyNameless/Untyped/CallByName.lean
+++ Cslib/Languages/LambdaCalculus/LocallyNameless/Untyped/CallByName.lean
@@ -45,11 +45,15 @@ lemma CBN.step_to_redex (step : M ⭢ₙ N) : M ↠βᶠ N := by
 
 /-- Call-by-Name reduction is contained in full β-reduction. -/
 lemma CBN.to_redex (step : M ↠ₙ N) : M ↠βᶠ N := by
-  induction step <;> grind [CBN.step_to_redex, Relation.ReflTransGen.trans]
+  induction step
+  · rfl
+  · grind [CBN.step_to_redex, Relation.ReflTransGen.trans]
 
 /-- Left congruence rule for application in Call-by-Name reduction. -/
 lemma CBN.steps_app_l_cong (step : M ↠ₙ M') (lc_N : LC N) : Term.app M N ↠ₙ Term.app M' N := by
-  induction step <;> grind [CBN.app]
+  induction step
+  · rfl
+  · grind [CBN.app]
 
 variable [HasFresh Var] [DecidableEq Var]
 
@@ -59,17 +63,23 @@ lemma CBN.lc_r (step : M ⭢ₙ N) : LC N := by
 
 /-- The right side of a Call-by-Name reduction is locally closed. -/
 lemma CBN.steps_lc_r (lc_M : LC M) (step : M ↠ₙ N) : LC N := by
-  induction step <;> grind [CBN.lc_r]
+  induction step
+  · exact lc_M
+  · grind [CBN.lc_r]
 
 /-- Substitution preserves a single Call-by-Name step. -/
 lemma CBN.step_subst (x : Var) (h : M ⭢ₙ M') (lc_N : LC N) :
     M[x := N] ⭢ₙ M'[x := N] := by
-  induction h <;> grind [Term.subst_open, Term.subst_lc, CBN.base, CBN.app]
+  induction h
+  · grind [Term.subst_open, CBN.base]
+  · grind [CBN.app]
 
 /-- Substitution preserves Call-by-Name reduction. -/
 lemma CBN.steps_subst (x : Var) (step : M ↠ₙ M') (lc_N : LC N) :
     M[x := N] ↠ₙ M'[x := N] := by
-  induction step <;> grind [CBN.step_subst]
+  induction step
+  · rfl
+  · grind [CBN.step_subst]
 
 end LambdaCalculus.LocallyNameless.Untyped.Term
 
