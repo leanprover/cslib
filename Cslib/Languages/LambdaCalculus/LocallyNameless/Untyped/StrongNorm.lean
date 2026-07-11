95ac53667346e9de5e30bb22368aff0404fc64c4
lyj <sjtu5140809011@gmail.com>
2026-07-12 05:09:51 +0800

    Update Cslib/Languages/LambdaCalculus/LocallyNameless/Untyped/StrongNorm.lean
    Co-authored-by: Chris Henson <46805207+chenson2018@users.noreply.github.com>

--- Cslib/Languages/LambdaCalculus/LocallyNameless/Untyped/StrongNorm.lean
+++ Cslib/Languages/LambdaCalculus/LocallyNameless/Untyped/StrongNorm.lean
@@ -133,11 +133,8 @@ lemma sn_abs_app_multiApp [DecidableEq Var] [HasFresh Var] {Ps} {M N : Term Var}
   | append_singleton Ps P ih =>
     rw [multiApp_tail]
     apply sn_app
-    · rw [multiApp_tail] at *
-      cases lc_MNPs
-      grind [sn_app_left]
-    · rw [multiApp_tail] at sn_MNPs
-      grind [sn_app_right]
+    · grind [cases LC, multiApp_tail, sn_app_left]
+    · grind [multiApp_tail, sn_app_right]
     · intro Q' P' hstep1 hstep2
       have ⟨M', N', Ps', h_M_red, h_N_red, h_Ps_red, h_cases⟩ := invert_abs_multiApp_mst hstep1
       rcases h_cases with h_P | ⟨h_st1, h_st2⟩
