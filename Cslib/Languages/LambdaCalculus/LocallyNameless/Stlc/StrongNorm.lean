95fb8c05f1cb03871d6090e50b525dd12b0d803a
lyj <sjtu5140809011@gmail.com>
2026-07-12 05:07:46 +0800

    Update Cslib/Languages/LambdaCalculus/LocallyNameless/Stlc/StrongNorm.lean
    Co-authored-by: Chris Henson <46805207+chenson2018@users.noreply.github.com>

--- Cslib/Languages/LambdaCalculus/LocallyNameless/Stlc/StrongNorm.lean
+++ Cslib/Languages/LambdaCalculus/LocallyNameless/Stlc/StrongNorm.lean
@@ -71,8 +71,7 @@ lemma semanticMap_saturated (τ : Ty Base) : @Saturated Var (semanticMap τ) := 
     · grind [sn_app_left (Var := Var) (N := fvar <| fresh {})]
     · grind
     · intro M N P _ _ _ s _
-      have := ih₂.multiApp M N (P ++ [s])
-      grind [multiApp_tail]
+      grind [ih₂.multiApp M N (P ++ [s]), multiApp_tail]
 
 /-- The `entailsContext` predicate ensures that each variable in the context
     is mapped to a term in the corresponding semantic map. -/
