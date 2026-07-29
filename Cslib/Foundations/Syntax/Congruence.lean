9bb01f834fa326d0378177b0530731c8ee4b6baf
Fabrizio Montesi <fm@fabriziomontesi.com>
2026-07-29 14:05:17 +0200

    unused arguments

--- Cslib/Foundations/Syntax/Congruence.lean
+++ Cslib/Foundations/Syntax/Congruence.lean
@@ -23,6 +23,7 @@ the expected congruence laws. -/
 class Congruence (r : α → α → Prop)
 
 /-- `a ≡[r] b` means that the `a` and `b` are related by the congruence `r`. -/
+@[nolint unusedArguments]
 def Congruence.r (r : α → α → Prop) [Congruence r] := r
 
 @[inherit_doc]
@@ -33,11 +34,13 @@ class DefaultCongruence (α : Type*) (r : outParam (α → α → Prop))
 
 /-- `a ≡ b` means that `a` and `b` are related by the canonical congruence relation for their
 type. -/
-def DefaultCongruence.r {α : Type*} {r : α → α → Prop} [DefaultCongruence α r] := r
+@[nolint unusedArguments]
+def DefaultCongruence.r {α : Type*} {r : α → α → Prop} [DefaultCongruence α r] (a b : α) := r a b
 
 @[inherit_doc]
 scoped infix:29 " ≡ " => DefaultCongruence.r
 
+@[nolint unusedArguments]
 instance (α : Type*) (r : α → α → Prop) [DefaultCongruence α r] : Congruence r := ⟨⟩
 
 /-- An equivalence relation on `α` preserved by all contexts. -/
