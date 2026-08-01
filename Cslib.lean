d19c32f2c25b970ea4d4bb8a108d8378b987165f
crei <crei.github@gmail.com>
2026-08-01 10:20:26 +0200

    Prove time upper-bounds for space-bounded classes.
    Proves bounds on the number of configurations reachable in bounded space
    and proves complexity-class inclusions from that:
    DSPACE(s) \subseteq DTIME(n * 2^(O(s))

--- Cslib.lean
+++ Cslib.lean
@@ -40,7 +40,11 @@ public import Cslib.Computability.Languages.MyhillNerode
 public import Cslib.Computability.Languages.OmegaLanguage
 public import Cslib.Computability.Languages.OmegaRegularLanguage
 public import Cslib.Computability.Languages.RegularLanguage
+public import Cslib.Computability.Machines.Turing.MultiTape.Classes
+public import Cslib.Computability.Machines.Turing.MultiTape.ConfigBound
 public import Cslib.Computability.Machines.Turing.MultiTape.Deterministic
+public import Cslib.Computability.Machines.Turing.MultiTape.SpaceInTime
+public import Cslib.Computability.Machines.Turing.MultiTape.TapeLemmas
 public import Cslib.Computability.Machines.Turing.SingleTape.Defs
 public import Cslib.Computability.Machines.Turing.SingleTape.Deterministic
 public import Cslib.Computability.Machines.Turing.SingleTape.NonDeterministic
