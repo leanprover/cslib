3ba57de1eba41f96c1d23fd5cbe865fb4ef86e06
crei <crei.github@gmail.com>
2026-08-02 15:17:48 +0200

    feat(MultiTapeTM): Some lemmas about Turing machine tapes (#768)
    Some useful lemmas about how the tapes are modified during the execution
    of Turing machines, some lemmas about halting behaviour and some about
    space usage (partly just moved).
    
    This has been split out from #767

--- Cslib.lean
+++ Cslib.lean
@@ -41,6 +41,7 @@ public import Cslib.Computability.Languages.OmegaLanguage
 public import Cslib.Computability.Languages.OmegaRegularLanguage
 public import Cslib.Computability.Languages.RegularLanguage
 public import Cslib.Computability.Machines.Turing.MultiTape.Deterministic
+public import Cslib.Computability.Machines.Turing.MultiTape.TapeLemmas
 public import Cslib.Computability.Machines.Turing.SingleTape.Defs
 public import Cslib.Computability.Machines.Turing.SingleTape.Deterministic
 public import Cslib.Computability.Machines.Turing.SingleTape.NonDeterministic
