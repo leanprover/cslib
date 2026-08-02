21d74b270fa7997f34643e0f835c4a563b0b511d
Fabrizio Montesi <fm@fabriziomontesi.com>
2026-08-02 15:06:25 +0200

    mech choreographies

--- Cslib.lean
+++ Cslib.lean
@@ -147,6 +147,7 @@ public import Cslib.Languages.LambdaCalculus.LocallyNameless.Untyped.Properties
 public import Cslib.Languages.LambdaCalculus.LocallyNameless.Untyped.StandardReduction
 public import Cslib.Languages.LambdaCalculus.LocallyNameless.Untyped.StrongNorm
 public import Cslib.Languages.LambdaCalculus.Named.Untyped.Basic
+public import Cslib.Languages.Mech.Choreography.Basic
 public import Cslib.Languages.StatefulProcesses.Basic
 public import Cslib.Languages.StatefulProcesses.Network
 public import Cslib.Logics.HML.Basic
