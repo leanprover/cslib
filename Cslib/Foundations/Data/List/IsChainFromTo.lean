cdff1eed1dc1e1ac7e3b78cf637d904a6bfeb25e
crei <crei.github@gmail.com>
2026-08-25 11:27:05 +0200

    Fix and apply suggestions.

--- Cslib/Foundations/Data/List/IsChainFromTo.lean
+++ Cslib/Foundations/Data/List/IsChainFromTo.lean
@@ -30,7 +30,7 @@ structure List.IsChainFromTo {α : Type*} (r : α → α → Prop) (chain : List
   ne_nil : chain ≠ []
   head_eq : chain.head ne_nil = a
   getLast_eq : chain.getLast ne_nil = b
-  
+
 attribute [grind →] List.IsChainFromTo.head_eq List.IsChainFromTo.getLast_eq
 
 /-- Create a `List.IsChainFromTo` from a non-empty `List.IsChain`. -/
@@ -39,39 +39,24 @@ theorem List.IsChainFromTo.of_isChain_ne_nil
     List.IsChainFromTo r chain (chain.head h_ne_nil) (chain.getLast h_ne_nil) :=
   ⟨hc, h_ne_nil, rfl, rfl⟩
 
-/-- Restatement of `head_eq`, but tagged with grind. -/
-@[grind →]
-lemma List.IsChainFromTo.head_eq (hc : chain.IsChainFromTo r a b) :
-    chain.head hc.ne_nil = a :=
-  hc.head
-
-/-- Restatement of `getLast_eq`, but tagged with grind. -/
-@[grind →]
-lemma List.IsChainFromTo.last_eq (hc : chain.IsChainFromTo r a b) :
-    chain.getLast hc.ne_nil = b :=
-  hc.last
-
 @[simp, grind ←]
 lemma List.IsChainFromTo.singleton {a : α} : List.IsChainFromTo r [a] a a :=
   ⟨List.IsChain.singleton a, by simp, rfl, rfl⟩
 
 /-- If there is an `r`-chain from `a` to `b` with duplicates, then there is a shorter `r`-chain
-from `a` to `b` (the one that skips the part between the duplicates).
-Note that applying this method iteratively does not necessarily lead to the shortest `r`-chain
-from `a` to `b`, since we always keep the initial and final segment. -/
+from `a` to `b` (the one that skips the part between the duplicates). -/
 lemma List.IsChainFromTo.exists_length_lt_of_not_nodup
     (hc : chain.IsChainFromTo r a b)
     (h_dup : ¬ chain.Nodup) :
     ∃ chain' : List α, chain'.IsChainFromTo r a b ∧ chain'.length < chain.length := by
-  rw [nodup_iff_getElem?_ne_getElem?] at h_dup
-  push Not at h_dup
+  simp only [nodup_iff_getElem?_ne_getElem?, not_forall, not_not] at h_dup
   obtain ⟨i, j, h_ij, h_lt, h_eq⟩ := h_dup
   use chain.take i ++ chain.drop j
-  constructor
-  · apply IsChainFromTo.mk ((hc.isChain.take _).append (hc.isChain.drop _) ?_)
-      (by simp; omega) (by grind) (by grind)
-    intro x hx y hy
-    rw [List.head?_drop] at hy
-    have := hc.isChain.getElem (i := i - 1)
-    grind
+  split_ands
+  · apply IsChainFromTo.mk ..
+    · apply (hc.isChain.take _).append (hc.isChain.drop _)
+      grind [List.head?_drop, hc.isChain.getElem (i := i - 1)]
+    · grind [append_eq_nil_iff, drop_eq_nil_iff]
+    · grind
+    · grind
   · grind
