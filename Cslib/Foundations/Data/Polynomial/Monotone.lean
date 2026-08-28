ab87f1b644b457135e35f1d731b19c43764d9d82
Bolton Bailey <bolton.bailey@gmail.com>
2026-08-28 13:14:54 -0700

    lemma

--- Cslib/Foundations/Data/Polynomial/Monotone.lean
+++ Cslib/Foundations/Data/Polynomial/Monotone.lean
@@ -1,7 +1,7 @@
 /-
-Copyright (c) 2026 Bolton Bailey. All rights reserved.
+Copyright (c) 2026 John Jennings. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
-Authors: Bolton Bailey, John Jennings
+Authors: John Jennings, Bolton Bailey
 -/
 
 module
