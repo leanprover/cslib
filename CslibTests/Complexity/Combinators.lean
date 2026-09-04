eae06220598bd4a1b85608cd9df711e62cc09d53
crei <crei.github@gmail.com>
2026-09-04 15:47:25 +0200

    Extend result to "almost constant" functions.

--- CslibTests/Complexity/Combinators.lean
+++ CslibTests/Complexity/Combinators.lean
@@ -4,7 +4,7 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Christian Reitwiessner
 -/
 
-import Cslib.Computability.Machines.Turing.MultiTape.Combinators.Finite
+import Cslib.Computability.Machines.Turing.MultiTape.Combinators.AlmostConstant
 
 namespace CslibTests
 
@@ -32,5 +32,16 @@ example : ∀ encIn encOut, ∃ c, EncodedComputableInTimeAndSpace
     (fun _ => c) (fun _ => 0) := by
   intro encIn encOut
   apply encodedComputableInTimeAndSpace_of_finite
+
+/-- Equality comparison to a constant is computable in constant time and zero space,
+also for infinite domains. -/
+example {α : Type*} [DecidableEq α] : ∀ encIn encOut out, ∃ c, EncodedComputableInTimeAndSpace
+    (encIn := encIn)
+    (encOut := encOut)
+    (fun a : α => a == out)
+    (fun _ => c) (fun _ => 0) := by
+  intro encIn encOut out
+  refine encodedComputableInTimeAndSpace_of_exists_finite_ne ⟨false, ?_⟩
+  exact Set.Finite.subset (Set.finite_singleton out) (by intro a ha; simp_all)
 
 end CslibTests
