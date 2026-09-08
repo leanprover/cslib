5ab53cd4c8491a7291d2f17f45e17f414279962f
Chi-Yun Hsu <chiyunhsu@users.noreply.github.com>
2026-09-08 16:44:33 -0700

    Update authors

--- Cslib/Computability/Languages/KleeneAlgorithm.lean
+++ Cslib/Computability/Languages/KleeneAlgorithm.lean
@@ -1,7 +1,7 @@
 /-
 Copyright (c) 2026 Brooke Gill and Chi-Yun Hsu. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
-Authors: Brooke Gill and Chi-Yun Hsu
+Authors: Brooke Gill, Chi-Yun Hsu
 -/
 
 module
