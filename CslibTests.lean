7b6da00adf7516709828c19b092967e9697b3d01
Samuel Schlesinger <SamuelSchlesinger@users.noreply.github.com>
2026-09-08 19:40:05 +0000

    feat(Crypto): Commitment Schemes (#766)
    Defines commitment schemes and their properties. Does not include
    computational hiding, but builds up to a proof that no statistically
    hiding, perfectly binding commitment scheme can exist. This justifies
    the existence of the computationally binding property, which we'll
    hopefully define in the future.

--- CslibTests.lean
+++ CslibTests.lean
@@ -2,6 +2,7 @@ import CslibTests.Bisimulation
 import CslibTests.CCS
 import CslibTests.CCS.VendingMachine
 import CslibTests.CLL
+import CslibTests.Commitment
 import CslibTests.Congruence
 import CslibTests.DFA
 import CslibTests.FreeMonad
