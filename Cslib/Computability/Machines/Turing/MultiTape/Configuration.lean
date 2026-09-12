c071d415c199a4e527b3bf0aa6fd6c5fdabda5e8
Aviv Bar Natan <46134216+barni120400@users.noreply.github.com>
2026-09-12 22:44:54 +0300

    refactor(MultiTapeTM): restore lemma placement

--- Cslib/Computability/Machines/Turing/MultiTape/Configuration.lean
+++ Cslib/Computability/Machines/Turing/MultiTape/Configuration.lean
@@ -1,7 +1,7 @@
 /-
 Copyright (c) 2026 Christian Reitwiessner. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
-Authors: Christian Reitwiessner, Aviv Bar Natan
+Authors: Christian Reitwiessner
 -/
 
 module
