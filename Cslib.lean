b09c73a5381ddc17b5818ca89df98b13edb4e7ba
Aviv Bar Natan <46134216+barni120400@users.noreply.github.com>
2026-09-12 21:23:39 +0300

    feat(MultiTapeTM): subloglog space is constant

--- Cslib.lean
+++ Cslib.lean
@@ -61,6 +61,7 @@ public import Cslib.Computability.Machines.Turing.MultiTape.Deterministic
 public import Cslib.Computability.Machines.Turing.MultiTape.DeterministicToNondeterministic
 public import Cslib.Computability.Machines.Turing.MultiTape.InputShortening
 public import Cslib.Computability.Machines.Turing.MultiTape.Nondeterministic
+public import Cslib.Computability.Machines.Turing.MultiTape.SpaceGap
 public import Cslib.Computability.Machines.Turing.MultiTape.TapeLemmas
 public import Cslib.Computability.Machines.Turing.SingleTape.Defs
 public import Cslib.Computability.Machines.Turing.SingleTape.Deterministic
