6d0efd679b9780d121d3cf9cc91547e2a2731c72
Thomas Krishna Waring <51426330+thomaskwaring@users.noreply.github.com>
2026-09-23 15:37:48 +0000

    chore(Foundations): move RelatesInSteps to Relation folder (#926)
    Co-authored-by: twwar <tom.waring@unimelb.edu.au>

--- Cslib/Computability/Machines/Turing/MultiTape/Deterministic.lean
+++ Cslib/Computability/Machines/Turing/MultiTape/Deterministic.lean
@@ -10,7 +10,7 @@ public import Mathlib.Algebra.Order.Group.Abs
 public import Mathlib.Algebra.Order.Group.Int
 public import Mathlib.Algebra.Order.BigOperators.Group.Finset
 public import Mathlib.Basic.Sign.Defs
-public import Cslib.Foundations.Data.RelatesInSteps
+public import Cslib.Foundations.Relation.RelatesInSteps
 public import Cslib.Computability.Machines.Turing.MultiTape.Configuration
 
 /-!
